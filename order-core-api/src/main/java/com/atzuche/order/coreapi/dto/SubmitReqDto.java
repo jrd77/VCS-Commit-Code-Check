dc2c604a13abd009bffd7189f9b857b570afb40f
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2019-12-13 14:51:07 +0800

    下单参数实体类。解决编译时，lombok找不到符号

--- order-core-api/src/main/java/com/atzuche/order/coreapi/dto/SubmitReqDto.java
+++ order-core-api/src/main/java/com/atzuche/order/coreapi/dto/SubmitReqDto.java
@@ -2,6 +2,8 @@ package com.atzuche.order.coreapi.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 /*
  * @Author ZhangBin
  * @Date 2019/12/12 15:14
@@ -10,5 +12,68 @@ import lombok.Data;
  **/
 @Data
 public class SubmitReqDto {
-
+    private String useSpecialPrice;
+    private String srvGetAddr;
+    private String srvReturnAddr;
+    private Integer source;
+    private String operator;
+    private String useAutoCoin;
+    private String srvGetLat;
+    private Integer srvGetFlag;
+    private Integer carNo;
+    private String sceneCode;
+    private String getCarFreeCouponId;
+    private String disCouponIds;
+    private String specialConsole;
+    private Integer useBal;
+    private String srvReturnLat;
+    private String token;
+    private String rentReason;
+    private String srvReturnLon;
+    private String offlineOrderStatus;
+    private Integer srvReturnFlag;
+    private Long revertTime;
+    private Integer oilType;
+    private String conPhone;
+    private Long rentTime;
+    private String srvGetLon;
+    private String abatement;
+    private MemInfoCache memInfoCache;
+    private MemBaseInfoVo tokenMemInfo;
+    private Integer memNo;
+    private Integer reqSource;
+    private String reqOs;
+    private Integer longitude;
+    private Integer latitude;
+    private String reqVersion;
+    private Integer isLeaveCity;
+    private String subSource;
+    private String cityCode;
+    private String requestId;
+    private String useAirportService;
+    private String schema;
+    private String ModuleName;
+    private String FunctionName;
+    private String publicCityCode;
+    private String IMEI;
+    private String OsVersion;
+    private String limitRedStatus;
+    private String AppVersion;
+    private String OS;
+    private String PublicLatitude;
+    private String mem_no;
+    private String freeDoubleTypeId;
+    private String rentCity;
+    private String deviceName;
+    private String publicToken;
+    private String carAddrIndex;
+    private String PublicLongitude;
+    private String queryId;
+    private List<ItemList> itemList;
+    private String reqIp;
+    private String mac;
+    private String AppChannelId;
+    private String appName;
+    private String AndroidId;
+    private String androidID;
 }
