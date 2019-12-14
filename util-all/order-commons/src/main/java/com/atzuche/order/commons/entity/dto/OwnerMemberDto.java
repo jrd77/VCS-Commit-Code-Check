605882ad8f8e25746f3c6abf2597199edd5484e4
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2019-12-14 18:11:06 +0800

    修改包名

---
+++ util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/dto/OwnerMemberDto.java
@@ -0,0 +1,39 @@
+package com.atzuche.order.commons.entity.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class OwnerMemberDto {
+    /**
+     * 会员号
+     */
+    private Integer memNo;
+    /**
+     * 会员类型
+     */
+    private Integer memType;
+    /**
+     * 手机号
+     */
+    private String phone;
+    /**
+     * 头像
+     */
+    private String headerUrl;
+    /**
+     * 真实姓名
+     */
+    private String realName;
+    /**
+     * 昵称
+     */
+    private String nickName;
+    /**
+     * 成功下单次数
+     */
+    private Integer orderSuccessCount;
+
+    private List<OwnerMemberRightDto> ownerMemberRightDtoList;
+}
---
+++ util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/dto/RenterMemberDto.java
@@ -0,0 +1,48 @@
+package com.atzuche.order.commons.entity.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class RenterMemberDto {
+    /**
+     * 主订单号
+     */
+    private Long orderNo;
+    /**
+     * 子订单号
+     */
+    private String renterOrderNo;
+    /**
+     * 会员号
+     */
+    private Integer memNo;
+    /**
+     * 手机号
+     */
+    private String phone;
+    /**
+     * 头像
+     */
+    private String headerUrl;
+    /**
+     * 真实姓名
+     */
+    private String realName;
+    /**
+     * 昵称
+     */
+    private String nickName;
+    /**
+     * 驾驶证初次领证日期
+     */
+    private LocalDateTime certificationTime;
+    /**
+     * 成功下单次数
+     */
+    private Integer orderSuccessCount;
+
+    private List<RenterMemberRightDto> renterMemberRightDtoList;
+}
