022d942d666a32ffdaddc2194d35ea9a607de255
闫海豹 <haibao.yan@atzuche.com>
2019-12-20 16:23:57 +0800

    收银台开发

---
+++ casier-all/cashier-account/src/main/java/com/atzuche/order/cashieraccount/vo/req/CashierRefundCallBackReqVO.java
@@ -0,0 +1,15 @@
+package com.atzuche.order.cashieraccount.vo.req;
+
+import com.autoyol.commons.web.ErrorCode;
+import lombok.Data;
+import org.springframework.util.Assert;
+
+/**
+ * 车辆押金/违章押金退款请求传参
+ * @author haibao.yan
+ */
+@Data
+public class CashierRefundCallBackReqVO {
+
+
+}
