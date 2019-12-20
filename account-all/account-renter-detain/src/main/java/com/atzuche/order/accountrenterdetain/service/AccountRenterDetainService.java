53e6fcf3cb53aa37839deb2d0f49c03898d73f79
闫海豹 <haibao.yan@atzuche.com>
2019-12-20 18:49:27 +0800

    暂扣开发

---
+++ account-all/account-renter-detain/src/main/java/com/atzuche/order/accountrenterdetain/service/AccountRenterDetainService.java
@@ -0,0 +1,44 @@
+package com.atzuche.order.accountrenterdetain.service;
+
+import com.atzuche.order.accountrenterdetain.service.notservice.AccountRenterDetainCostNoTService;
+import com.atzuche.order.accountrenterdetain.service.notservice.AccountRenterDetainDetailNoTService;
+import com.atzuche.order.accountrenterdetain.vo.req.DetainRenterDepositReqVO;
+import com.autoyol.commons.web.ErrorCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+
+/**
+ * 暂扣费用总表
+ *
+ * @author ZhangBin
+ * @date 2019-12-11 17:51:17
+ */
+@Service
+public class AccountRenterDetainService {
+    @Autowired
+    private AccountRenterDetainCostNoTService accountRenterDetainCostNoTService;
+    @Autowired
+    private AccountRenterDetainDetailNoTService accountRenterDetainDetailNoTService;
+
+    /**
+     * 查询暂扣费用总和
+     */
+    public int getRenterDetainAmt(String orderNo ,String memNo){
+        return accountRenterDetainCostNoTService.getRenterDetainAmt(orderNo,memNo);
+    }
+
+    /**
+     * 暂扣费用资金进出
+     */
+    public void changeRenterDetainCost(DetainRenterDepositReqVO detainRenterDeposit){
+        //1校验
+        Assert.notNull(detainRenterDeposit, ErrorCode.PARAMETER_ERROR.getText());
+        detainRenterDeposit.check();
+        //2 更新违章押金总额
+        accountRenterDetainCostNoTService.changeRenterDetainCost(detainRenterDeposit);
+        //3 记录违章费用账户费用流水
+        accountRenterDetainDetailNoTService.insertCostDetail(detainRenterDeposit);
+    }
+}
---
+++ account-all/account-renter-detain/src/main/java/com/atzuche/order/accountrenterdetain/vo/req/DetainRenterDepositReqVO.java
@@ -0,0 +1,67 @@
+package com.atzuche.order.accountrenterdetain.vo.req;
+
+import com.atzuche.order.commons.enums.RenterCashCodeEnum;
+import com.autoyol.commons.web.ErrorCode;
+import lombok.Data;
+import org.springframework.util.Assert;
+
+import java.time.LocalDateTime;
+
+/**
+ * 暂扣请求参数
+ */
+@Data
+public class DetainRenterDepositReqVO {
+    /**
+     * 主订单号
+     */
+    private String orderNo;
+    /**
+     * 会员号
+     */
+    private String memNo;
+    /**
+     * 暂扣金额
+     */
+    private Integer amt;
+    /**
+     * 暂扣时间
+     */
+    private LocalDateTime time;
+    /**
+     *暂扣来源
+     */
+    private RenterCashCodeEnum renterCashCodeEnum;
+    /**
+     * 暂扣凭证
+     */
+    private String uniqueNo;
+    /**
+     * 备注
+     */
+    private String remake;
+
+    /**
+     * 创建人
+     */
+    private String createOp;
+
+    /**
+     * 更新人
+     */
+    private String updateOp;
+
+    /**
+     * 参数校验
+     */
+    public void check() {
+        Assert.notNull(getMemNo(), ErrorCode.PARAMETER_ERROR.getText());
+        Assert.notNull(getOrderNo(), ErrorCode.PARAMETER_ERROR.getText());
+        Assert.notNull(getRenterCashCodeEnum(), ErrorCode.PARAMETER_ERROR.getText());
+        Assert.notNull(getAmt(), ErrorCode.PARAMETER_ERROR.getText());
+        Assert.isTrue(getAmt()!=0, ErrorCode.PARAMETER_ERROR.getText());
+        Assert.notNull(getUniqueNo(), ErrorCode.PARAMETER_ERROR.getText());
+
+
+    }
+}
