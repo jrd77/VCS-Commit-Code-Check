75b94401fe6f3308e3af2622d9c972bc7a68e8f7
闫海豹 <haibao.yan@atzuche.com>
2019-12-25 14:57:41 +0800

    收银台开发  字段调整

--- casier-all/cashier-account/src/main/java/com/atzuche/order/cashieraccount/config/OrderPayCallBackRabbitConfig.java
+++ casier-all/cashier-account/src/main/java/com/atzuche/order/cashieraccount/config/OrderPayCallBackRabbitConfig.java
@@ -1,9 +1,10 @@
 package com.atzuche.order.cashieraccount.config;
 
 import com.atzuche.order.cashieraccount.service.CashierPayService;
+import com.atzuche.order.cashieraccount.vo.res.pay.OrderPayAsynResVO;
 import com.atzuche.order.commons.CatConstants;
-import com.atzuche.order.commons.enums.RabbitBusinessTypeEnum;
 import com.atzuche.order.commons.service.RabbitMsgLogService;
+import com.autoyol.commons.utils.GsonUtils;
 import com.dianping.cat.Cat;
 import com.dianping.cat.message.Transaction;
 import com.rabbitmq.client.Channel;
@@ -33,16 +34,27 @@ public class OrderPayCallBackRabbitConfig {
     )
     )
     @RabbitHandler
-    public void payCallBackAsyn(Message message, Channel channel){
-        log.info("OrderPayCallBack payCallBackAsyn start param;[{}]", message);
+    public void payCallBack(Message message, Channel channel){
+        log.info("OrderPayCallBack payCallBack start param;[{}]", message);
+        Transaction t = Cat.getProducer().newTransaction(CatConstants.RABBIT_MQ_CALL, "支付系统rabbitMQ异步回调payCallBack");
+
         try {
             String orderPayAsynStr = new String(message.getBody());
-            rabbitMsgLogService.insertRabbitMsgLog(message,RabbitBusinessTypeEnum.ORDER_PAY_CALL_BACK,orderPayAsynStr);
-            cashierPayService.payCallBackAsyn(orderPayAsynStr);
+            Cat.logEvent(CatConstants.RABBIT_MQ_METHOD,"OrderPayCallBackRabbitConfig.payCallBack");
+            Cat.logEvent(CatConstants.RABBIT_MQ_PARAM,orderPayAsynStr);
+
+            OrderPayAsynResVO orderPayAsynVO = GsonUtils.convertObj(orderPayAsynStr, OrderPayAsynResVO.class);
+            rabbitMsgLogService.insertRabbitMsgLog(message,orderPayAsynVO.getPayType(),orderPayAsynStr,orderPayAsynVO.getQn());
+            cashierPayService.payCallBackAsyn(orderPayAsynVO);
+            t.setStatus(Transaction.SUCCESS);
         } catch (Exception e) {
-            log.error("OrderPayCallBack payCallBackAsyn,e={},message={}",e,message);
+            log.error("OrderPayCallBack payCallBack,e={},message={}",e,message);
+            t.setStatus(e);
+            Cat.logError("MQ 接收 支付系统回调 失败",e);
+        } finally {
+            t.complete();
         }
-        log.info("OrderPayCallBack payCallBackAsyn end " );
+        log.info("OrderPayCallBack payCallBack end " );
     }
 
 
