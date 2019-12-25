6d8a1562ab412fa89fc285b0e7fc29cfcc08b80d
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2019-12-25 15:05:53 +0800

    子订单费用代码

--- order-core-all/renter-all/renter-mem/src/main/java/com/atzuche/order/rentermem/service/RenterMemberRightService.java
+++ order-core-all/renter-all/renter-mem/src/main/java/com/atzuche/order/rentermem/service/RenterMemberRightService.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Service;
 import com.atzuche.order.rentermem.mapper.RenterMemberRightMapper;
 
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 /**
@@ -31,24 +33,38 @@ public class RenterMemberRightService{
      * @param wzDepositAmt 违章押金
      * @return 如果CarDepositAmt和wzDepositAmt都不为null，优先选择车辆押金计算
      */
-    public int calDepositAmt(List<RenterMemberRightDTO> renterMemberRightDTOList, Integer carDepositAmt, Integer wzDepositAmt){
+    public int calMemRightDepositAmt(List<RenterMemberRightDTO> renterMemberRightDTOList, Integer carDepositAmt, Integer wzDepositAmt){
         if(carDepositAmt == null && wzDepositAmt==null){
             CalDepositAmtException calDepositAmtException = new CalDepositAmtException();
             Cat.logError(calDepositAmtException);
             throw calDepositAmtException;
         }
-        List<RenterMemberRightDTO> collect = renterMemberRightDTOList
+        List<RenterMemberRightDTO> staff = renterMemberRightDTOList
                 .stream()
                 .filter(x -> RenterMemRightEnum.STAFF.getRightCode().equals(x.getRightCode()))
                 .limit(1)
                 .collect(Collectors.toList());
-        if(collect!=null && collect.size()==1){
+        List<RenterMemberRightDTO> taskList = renterMemberRightDTOList
+                .stream()
+                .filter(x -> (
+                        RenterMemRightEnum.BIND_WECHAT.getRightCode().equals(x.getRightCode())
+                                || RenterMemRightEnum.INVITE_FRIENDS.getRightCode().equals(x.getRightCode())
+                                || RenterMemRightEnum.SUCCESS_RENTCAR.getRightCode().equals(x.getRightCode())
+                                || RenterMemRightEnum.MEMBER_LEVEL.getRightCode().equals(x.getRightCode())))
+                .collect(Collectors.toList());
+
+        if(staff!=null && staff.size()==1){
             if(carDepositAmt != null){
                 return GlobalConstant.MEMBER_RIGHT_STAFF_CAR_DEPOSIT;
             }else if(wzDepositAmt != null){
                 return GlobalConstant.MEMBER_RIGHT_STAFF_WZ_DEPOSIT;
             }
-        }else{
+        }/*else if(taskStream != null && taskStream.count()>0){
+            taskStream.collect(Coll)
+            if(carDepositAmt != null){
+                return
+            }
+        }*/else{
             if(carDepositAmt != null){
                 return carDepositAmt;
             }else if(wzDepositAmt != null){
@@ -59,5 +75,7 @@ public class RenterMemberRightService{
         Cat.logError(calDepositAmtException);
         throw calDepositAmtException;
     }
+
+
 
 }
