9faea21782204573f413fe15b949c5009fb0a12d
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2019-12-26 11:05:55 +0800

    取还车费用

--- order-core-all/renter-all/renter-order/src/main/java/com/atzuche/order/renterorder/service/RenterOrderCalCostService.java
+++ order-core-all/renter-all/renter-order/src/main/java/com/atzuche/order/renterorder/service/RenterOrderCalCostService.java
@@ -83,6 +83,7 @@ public class RenterOrderCalCostService {
         GetReturnCarCostReqDto getReturnCarCostReqDto = renterOrderCostReqDTO.getGetReturnCarCostReqDto();
         getReturnCarCostReqDto.setSumJudgeFreeFee(rentAmt + insurAmt + serviceAmount);
         GetReturnCostDto returnCarCost = renterOrderCostCombineService.getReturnCarCost(getReturnCarCostReqDto);
+        Integer getReturnAmt = returnCarCost.getRenterOrderCostDetailEntityList().stream().collect(Collectors.summingInt(RenterOrderCostDetailEntity::getTotalAmount));
         detailList.addAll(returnCarCost.getRenterOrderCostDetailEntityList());
         subsidyList.addAll(returnCarCost.getRenterOrderSubsidyDetailEntityList());
 
@@ -90,7 +91,7 @@ public class RenterOrderCalCostService {
         //获取取还车超运能费用 TODO
 
         //租车费用 = 租金+平台保障费+全面保障费+取还车费用+取还车超云能费用+附加驾驶员费用+手续费；
-        int rentCarAmount = rentAmt + insurAmt + comprehensiveEnsureAmount + 0 + 0 + totalAmount + serviceAmount;
+        int rentCarAmount = rentAmt + insurAmt + comprehensiveEnsureAmount + getReturnAmt + 0 + totalAmount + serviceAmount;
 
         renterOrderCostRespDTO.setRentCarAmount(rentCarAmount);
         renterOrderCostRespDTO.setRenterOrderCostDetailDTOList(detailList);
