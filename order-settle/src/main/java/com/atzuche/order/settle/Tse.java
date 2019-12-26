ad7eb132ba074834c24753689e464af7404e95f8
闫海豹 <haibao.yan@atzuche.com>
2019-12-26 20:43:07 +0800

    支付收银台开发

--- order-settle/src/main/java/com/atzuche/order/settle/Tse.java
+++ order-settle/src/main/java/com/atzuche/order/settle/Tse.java
@@ -29,7 +29,7 @@ public class Tse {
 
 //
 //		//私钥加密
-        String AUTO_TRANS_PRIVATE_KEY = "xxx";
+        String AUTO_TRANS_PRIVATE_KEY = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAI7uBjKrIcBlBOc6LAimLHhbRZJv+K9LdBg9alFC92eTEieCJ70p2qI8BfbOKs11T2hqyz0hJd13kCPWajZQJR2L8ytitnXFP5cAdZLaZznZ6avUzE1J0abgl5n/cel1hANHebPXhB0s1a81d1zBgcfTDkY2jqop2F+L3WVWqmwTAgMBAAECgYA/AZpl156nS2Df9XrXzTkpTULcDNQZoi9pSBmH/PdHB0Qnem6+KqcggUk3xSaFL8NzijBRoD0q5bv6sFhd3PKe601WSQjmPL9NUNclP0UDGPps7tIkyOphoU7P26lZja5Bc/doC/fd93zSCCQvMJ9q9yP0R5t0sU/8zmSpHuDPgQJBAN2EkW+mzpt8sOx/BddEj/bwpiZw6FFFIXHzL7S4eS9s7BIfipfQD/tgVFnZpPLFAGHV0ReM4L7aEgxwXuyGYvMCQQClLb21v7a1dA0egPrMfrXif5tad6W8owqi7+b7gyTkLPe/yrvRjzUOfDM0UJQN6ZE08EL4exgLn4W1ttxhlNphAkEAi+1PyU1/OSy8vcdHM0H2BrDg42ty9NNMWySYv/m/YzvUq2YNL/SYA9xHrDIVRd03bpWX7N5qoWQy/nSFIbeKDQJABRLTmeqKlSY8ZOx3R6K4uX1L60pzh8jGQdz2jlEn5+5NqdFzVWt9qLU96WR0N70t3Z9nnzRAt9Kti7hTKpgk4QJAcAkr0uDuHo86i8Nk1Nwwi4Q1ATbSeGp8x1rJISFb4xrO3KGGyjkYTfm0I49JxFXwpZYRIgdfhtHqwhDTLzkCPg==";
         String atpaySign =  privateKeySignature(AUTO_TRANS_PRIVATE_KEY, reqContent);
         //单独传递
         System.out.println("签名串atpaysign="+atpaySign);
