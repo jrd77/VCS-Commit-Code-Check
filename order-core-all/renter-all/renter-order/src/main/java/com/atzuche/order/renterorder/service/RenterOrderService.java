513e8a4e20d47561ea4d308d616a67db873c53ef
seazhao <haishui.zhao@atzuche.cn>
2019-12-26 10:20:43 +0800

    #修改订单

--- order-core-all/renter-all/renter-order/src/main/java/com/atzuche/order/renterorder/service/RenterOrderService.java
+++ order-core-all/renter-all/renter-order/src/main/java/com/atzuche/order/renterorder/service/RenterOrderService.java
@@ -32,5 +32,34 @@ public class RenterOrderService{
     public RenterOrderEntity getRenterOrderByOrderNoAndIsEffective(String orderNo) {
     	return renterOrderMapper.getRenterOrderByOrderNoAndIsEffective(orderNo);
     }
+    
+    
+    /**
+     * 获取租客子单根据租客子单号
+     * @param renterOrderNo 租客子订单号
+     * @return RenterOrderEntity
+     */
+    public RenterOrderEntity getRenterOrderByRenterOrderNo(String renterOrderNo) {
+    	return renterOrderMapper.getRenterOrderByRenterOrderNo(renterOrderNo);
+    }
+    
+    /**
+     * 修改租客子订单是否有效状态
+     * @param id
+     * @param effectiveFlag
+     * @return Integer
+     */
+    public Integer updateRenterOrderEffective(Integer id, Integer effectiveFlag) {
+    	return renterOrderMapper.updateRenterOrderEffective(id, effectiveFlag);
+    }
+    
+    /**
+     * 保存租客子订单
+     * @param renterOrderEntity
+     * @return Integer
+     */
+    public Integer saveRenterOrder(RenterOrderEntity renterOrderEntity) {
+    	return renterOrderMapper.insertSelective(renterOrderEntity);
+    }
 
 }
