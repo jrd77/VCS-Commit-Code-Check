396889499eb61f1d6a4d74eb78d75f7017df9755
胡春林 <445777020@qq.com>
2019-12-28 16:54:35 +0800

    交接车/配送服务

---
+++ order-delivery/src/main/java/com/atzuche/delivery/common/DeliveryErrorCode.java
@@ -0,0 +1,27 @@
+package com.atzuche.delivery.common;
+
+/**
+ * @author 胡春林
+ * 配送信息错误编码
+ */
+public enum  DeliveryErrorCode{
+
+    SEND_REN_YUN_HTTP_ERROR("30001","请求仁云接口出错"),
+    ;
+
+    private String code;
+    private String text;
+
+     DeliveryErrorCode(String code, String text) {
+        this.code = code;
+        this.text = text;
+    }
+
+    public String getValue() {
+        return code;
+    }
+
+    public String getName() {
+        return text;
+    }
+}
