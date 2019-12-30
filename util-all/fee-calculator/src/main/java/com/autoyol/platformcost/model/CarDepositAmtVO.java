c1c972f7bca36516b6c07dd845721e548f6001d8
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2019-12-30 11:53:40 +0800

    修改租车押金

--- util-all/fee-calculator/src/main/java/com/autoyol/platformcost/model/CarDepositAmtVO.java
+++ util-all/fee-calculator/src/main/java/com/autoyol/platformcost/model/CarDepositAmtVO.java
@@ -1,5 +1,8 @@
 package com.autoyol.platformcost.model;
 
+import lombok.Data;
+
+@Data
 public class CarDepositAmtVO {
 
 	/**
@@ -9,38 +12,18 @@ public class CarDepositAmtVO {
 	/**
 	 * 减免比例
 	 */
-	private Double reliefPercetage;
+	//private Double reliefPercetage;
 	/**
 	 * 押金系数
 	 */
 	private Double carDepositRadio;
-	/**
-	 * 减免金额
-	 */
-	private Integer reliefAmt;
-	public Integer getCarDepositAmt() {
-		return carDepositAmt;
-	}
-	public void setCarDepositAmt(Integer carDepositAmt) {
-		this.carDepositAmt = carDepositAmt;
-	}
-	public Double getReliefPercetage() {
-		return reliefPercetage;
-	}
-	public void setReliefPercetage(Double reliefPercetage) {
-		this.reliefPercetage = reliefPercetage;
-	}
-	public Double getCarDepositRadio() {
-		return carDepositRadio;
-	}
-	public void setCarDepositRadio(Double carDepositRadio) {
-		this.carDepositRadio = carDepositRadio;
-	}
-	public Integer getReliefAmt() {
-		return reliefAmt;
-	}
-	public void setReliefAmt(Integer reliefAmt) {
-		this.reliefAmt = reliefAmt;
-	}
+    /**
+     * 车型品牌系数
+     */
+    private Double carBrandTypeRadio;
+    /**
+     * 车辆年份系数
+     */
+    private Double carYearRadio;
 	
 }
