33757454515a5738bcf32b3acdbe7328a7ac0f28
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2019-12-31 15:46:52 +0800

    修改异常信息类

--- order-core-api/src/main/java/com/atzuche/order/coreapi/service/SubmitOrderService.java
+++ order-core-api/src/main/java/com/atzuche/order/coreapi/service/SubmitOrderService.java
@@ -45,9 +45,6 @@ public class SubmitOrderService {
     private MemberService memberService;
     @Autowired
     private CarService carService;
-    @Autowired
-    private CarDetailQueryFeignApi carDetailQueryFeignApi;
-
     @Resource
     private UniqueOrderNoService uniqueOrderNoService;
     @Resource
@@ -64,67 +61,6 @@ public class SubmitOrderService {
     private OwnerGoodsService ownerGoodsService;
     @Autowired
     private OwnerOrderService ownerOrderService;
-
-    public ResponseData submitOrder(SubmitOrderReq submitReqDto) {
-        //调用日志模块 TODO
-
-        try {
-            OrderContextDTO orderContextDto = new OrderContextDTO();
-            //获取租客商品信息
-            RenterGoodsDetailDTO renterGoodsDetailDto = carService.getRenterGoodsDetail(null);
-            //获取车主商品信息
-//            OwnerGoodsDetailDTO ownerGoodsDetailDto = carService.getOwnerGoodsDetail(renterGoodsDetailDto);
-            //获取车主会员信息
-            OwnerMemberDTO ownerMemberDto = memberService.getOwnerMemberInfo(renterGoodsDetailDto.getOwnerMemNo());
-            //获取租客会员信息
-            //RenterMemberDTO renterMemberDto = memberService.getRenterMemberInfo(submitReqDto.getMemNo());
-
-            //组装数据
-            orderContextDto.setRenterGoodsDetailDto(renterGoodsDetailDto);
-//            orderContextDto.setOwnerGoodsDetailDto(ownerGoodsDetailDto);
-            orderContextDto.setOwnerMemberDto(ownerMemberDto);
-           // orderContextDto.setRenterMemberDto(renterMemberDto);
-
-
-            //开始校验规则 （前置校验 + 风控）TODO
-//            submitOrderFilterService.checkRules(submitReqDto,orderContextDto);
-
-
-            //调用费用计算模块,组装数据orderContextDto TODO
-
-
-            //车主券抵扣,组装数据orderContextDto TODO
-
-            //限时红包抵扣,组装数据orderContextDto TODO
-
-            //优惠券抵扣,组装数据orderContextDto TODO
-
-            //凹凸比抵扣,组装数据orderContextDto TODO
-
-            //钱包抵扣,组装数据orderContextDto TODO
-        } catch (OrderException ex) {
-            String errorCode = ex.getErrorCode();
-            String errorMsg = ex.getErrorMsg();
-            LOGGER.error("下单失败", ex);
-            return ResponseData.createErrorCodeResponse(errorCode, errorMsg);
-        } catch (Exception ex) {
-            LOGGER.error("下单异常", ex);
-            return ResponseData.createErrorCodeResponse(ErrorCode.FAILED.getCode(), ErrorCode.FAILED.getText());
-        }
-
-
-//====================落库OrderContextDto=========================
-
-        //调用主订单模块
-
-        //调用租客模块 (子订单模块、商品、会员、交易流程)
-
-        //调用取送车模块
-
-        //调用车主模块（子订单模块、商品、会员）
-        return null;
-    }
-
 
     /**
      * 提交订单
