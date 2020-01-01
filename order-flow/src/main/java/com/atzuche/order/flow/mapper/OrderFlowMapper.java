3b149d989741c40594bf1a516d5b889689a2902f
pengcheng.fu <pengcheng.fu@atzuche.com>
2020-01-01 15:17:28 +0800

    add order flow

---
+++ order-flow/src/main/java/com/atzuche/order/flow/mapper/OrderFlowMapper.java
@@ -0,0 +1,25 @@
+package com.atzuche.order.flow.mapper;
+
+import com.atzuche.order.flow.entity.OrderFlowEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 租客端交易流程表
+ * 
+ * @author ZhangBin
+ * @date 2020-01-01 15:10:51
+ */
+@Mapper
+public interface OrderFlowMapper{
+
+    OrderFlowEntity selectByPrimaryKey(Integer id);
+
+    int insert(OrderFlowEntity record);
+    
+    int insertSelective(OrderFlowEntity record);
+
+    int updateByPrimaryKey(OrderFlowEntity record);
+    
+    int updateByPrimaryKeySelective(OrderFlowEntity record);
+
+}
