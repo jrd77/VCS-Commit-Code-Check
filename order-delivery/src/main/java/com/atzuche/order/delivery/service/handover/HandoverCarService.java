d80872a8cb95e10b85275fa06a1d16f25138cd1f
胡春林 <445777020@qq.com>
2020-01-01 15:38:46 +0800

    交接车/配送服务

--- order-delivery/src/main/java/com/atzuche/order/delivery/service/handover/HandoverCarService.java
+++ order-delivery/src/main/java/com/atzuche/order/delivery/service/handover/HandoverCarService.java
@@ -5,23 +5,13 @@ import com.atzuche.order.delivery.entity.OwnerHandoverCarInfoEntity;
 import com.atzuche.order.delivery.entity.OwnerHandoverCarRemarkEntity;
 import com.atzuche.order.delivery.entity.RenterHandoverCarInfoEntity;
 import com.atzuche.order.delivery.entity.RenterHandoverCarRemarkEntity;
-import com.atzuche.order.delivery.enums.HandoverCarTypeEnum;
-import com.atzuche.order.delivery.enums.ServiceTypeEnum;
 import com.atzuche.order.delivery.enums.UserTypeEnum;
 import com.atzuche.order.delivery.exception.HandoverCarOrderException;
 import com.atzuche.order.delivery.mapper.*;
-import com.atzuche.order.delivery.service.CarService;
-import com.atzuche.order.delivery.service.MemberService;
-import com.atzuche.order.delivery.service.OrderService;
-import com.atzuche.order.delivery.service.SendImMsgThirdService;
-import com.atzuche.order.delivery.vo.CarBO;
-import com.atzuche.order.delivery.vo.HandoverCarRenYunVO;
-import com.atzuche.order.delivery.vo.OrderInfoVO;
 import com.atzuche.order.delivery.vo.handover.HandoverCarVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,20 +26,6 @@ import java.util.Objects;
 @Slf4j
 public class HandoverCarService {
 
-    @Autowired
-    SendImMsgThirdService sendImMsgThirdService;
-
-    @Autowired
-    CarService carService;
-
-    @Autowired
-    DelegationCarAdminMapper delegationCarAdminMapper;
-
-    @Autowired
-    MemberService memberService;
-
-    @Autowired
-    OrderService orderService;
     @Resource
     RenterHandoverCarInfoMapper renterHandoverCarInfoMapper;
     @Resource
@@ -60,88 +36,12 @@ public class HandoverCarService {
     OwnerHandoverCarRemarkMapper ownerHandoverCarRemarkMapper;
 
     /**
-     * 向租客和车主发送交接车信息
-     */
-    public void handlerHandoverCarStepByTransInfo(HandoverCarRenYunVO handoverCarVO) {
-
-        if (StringUtils.isBlank(handoverCarVO.getProId()) || !handoverCarVO.isUserType()) {
-            return;
-        }
-        //todo 根据订单号查询订单获取车辆号 租客 车主 来源等数据 Trans trans = transProgressService.queryMemInfo(orderNo);
-        OrderInfoVO orderInfoVO = new OrderInfoVO();
-        int memNo;
-        int userType = Integer.valueOf(handoverCarVO.getUserType());
-        memNo = userType == 1 ? orderInfoVO.getRenterNo() : orderInfoVO.getOwnerNo();
-        int proId = Integer.valueOf(handoverCarVO.getProId());
-        String orderNoStr = String.valueOf(handoverCarVO.getOrderNo());
-        if (memNo != 0 && (proId == 2 || proId == 6)
-                && !("202".equals(orderInfoVO.getSource())
-                || "400".equals(orderInfoVO.getSource())
-                || "401".equals(orderInfoVO.getSource()))) {
-            if (userType == UserTypeEnum.RENTER_TYPE.getValue().intValue()) {
-                if (orderInfoVO.isGreaterThanZero()) {
-                    log.info("租客订单号:renterOrderNo：{}", orderInfoVO.getRenterOrderNo());
-                    orderNoStr = orderInfoVO.getRenterOrderNo().toString();
-                }
-                log.info("renterTransInfo: orderNo={}, renterOrderNo={}", handoverCarVO.getOrderNo(), orderInfoVO.getRenterOrderNo());
-                if (handoverCarVO.getServiceType().equals(ServiceTypeEnum.TAKE_TYPE.getValue())) {
-                    sendImMsgThirdService.sendImMsg9798("97", String.valueOf(memNo), orderNoStr, false, "", handoverCarVO.getHeadName(), handoverCarVO.getHeadPhone());
-                } else if (handoverCarVO.getServiceType().equals(ServiceTypeEnum.BACK_TYPE.getValue())) {
-                    sendImMsgThirdService.sendImMsg9798("98", String.valueOf(memNo), orderNoStr, false, "", handoverCarVO.getHeadName(), handoverCarVO.getHeadPhone());
-                }
-            } else if (userType == UserTypeEnum.OWNER_TYPE.getValue().intValue()) {
-                String toMemNo = String.valueOf(memNo);
-                CarBO carInfo = carService.getCarInfoByCarNo(orderInfoVO.getCarNo());
-                int ownerType = carInfo.getOwnerType();
-                if (ownerType == 35) {
-                    //查询代管车管理员
-                    Long delegatMobile = delegationCarAdminMapper.getAdminMobileByCarNo(orderInfoVO.getCarNo());
-                    if (delegatMobile != null) {
-                        toMemNo = memberService.getMemNoByMobile(delegatMobile.toString()).toString();
-                    }
-                }
-                if (handoverCarVO.getServiceType().equals(ServiceTypeEnum.TAKE_TYPE.getValue())) {
-                    sendImMsgThirdService.sendImMsg130("130", toMemNo, String.valueOf(orderInfoVO.getOrderNo()), true, String.valueOf(orderInfoVO.getCarNo()), handoverCarVO.getHeadName(), handoverCarVO.getHeadPhone());
-                } else if (handoverCarVO.getServiceType().equals(ServiceTypeEnum.BACK_TYPE.getValue())) {
-                    sendImMsgThirdService.sendImMsg131("131", toMemNo, String.valueOf(orderInfoVO.getOrderNo()), true, String.valueOf(orderInfoVO.getCarNo()), handoverCarVO.getHeadName(), handoverCarVO.getHeadPhone());
-                }
-            }
-        }
-
-        if (memNo != 0 && ((proId == 4 && (handoverCarVO.getDescription().equals("车辆已交给租客") || handoverCarVO.getDescription().equals("已同租客交接完成"))) ||
-                (proId == 8 && (handoverCarVO.getDescription().equals("车辆已交还给车主") || handoverCarVO.getDescription().equals("已同车主交接完成"))))
-                && !("202".equals(orderInfoVO.getSource()) || "400".equals(orderInfoVO.getSource()) || "401".equals(orderInfoVO.getSource()))) {
-            if (userType == UserTypeEnum.RENTER_TYPE.getValue().intValue()) {
-                if (orderInfoVO.isGreaterThanZero()) {
-                    log.info("租客订单号:renterOrderNo：{}", orderInfoVO.getRenterOrderNo());
-                    orderNoStr = orderInfoVO.getRenterOrderNo().toString();
-                }
-                if (handoverCarVO.getServiceType().equals(ServiceTypeEnum.TAKE_TYPE.getValue())) {
-                    log.info("take car sendImMsg94 userType 1");
-                    sendImMsgThirdService.sendImMsg94(String.valueOf(memNo), orderNoStr, "true", false, "");
-                } else if (handoverCarVO.getServiceType().equals(ServiceTypeEnum.BACK_TYPE.getValue())) {
-                    log.info("back car sendImMsg273 userType 1");
-                    sendImMsgThirdService.sendImMsg273(String.valueOf(memNo), orderNoStr, "true", false, "");
-                }
-            } else if (userType == UserTypeEnum.OWNER_TYPE.getValue().intValue()) {
-                if (handoverCarVO.getServiceType().equals(ServiceTypeEnum.TAKE_TYPE.getValue())) {
-                    log.info("take car sendImMsg94 userType 2");
-                    sendImMsgThirdService.sendImMsg94(String.valueOf(memNo), orderNoStr, "false", true, String.valueOf(orderInfoVO.getCarNo()));
-                } else if (handoverCarVO.getServiceType().equals(ServiceTypeEnum.BACK_TYPE.getValue())) {
-                    log.info("back car sendImMsg273 userType 2");
-                    sendImMsgThirdService.sendImMsg273(String.valueOf(memNo), orderNoStr, "false", true, String.valueOf(orderInfoVO.getCarNo()));
-                }
-            }
-        }
-    }
-
-    /**
      * 新增交接车数据
      *
      * @param handoverCarVO
      */
     @Transactional(rollbackFor = Exception.class)
-    public void addHandoverCarInfo(HandoverCarVO handoverCarVO,int userType) {
+    public void addHandoverCarInfo(HandoverCarVO handoverCarVO, int userType) {
         if (Objects.isNull(handoverCarVO) || handoverCarVO.getHandoverCarInfoDTO().getType() == null) {
             throw new HandoverCarOrderException(DeliveryErrorCode.DELIVERY_PARAMS_ERROR);
         }
@@ -173,6 +73,7 @@ public class HandoverCarService {
 
     /**
      * 根据消息ID获取
+     *
      * @param msgId
      * @return
      */
