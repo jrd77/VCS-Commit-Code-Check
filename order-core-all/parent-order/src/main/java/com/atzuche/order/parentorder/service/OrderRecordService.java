cf92bd72a948402f545b8dce3566e4cdacb8155b
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-01-01 17:25:37 +0800

    增加日志信息

--- order-core-all/parent-order/src/main/java/com/atzuche/order/parentorder/service/OrderRecordService.java
+++ order-core-all/parent-order/src/main/java/com/atzuche/order/parentorder/service/OrderRecordService.java
@@ -1,5 +1,6 @@
 package com.atzuche.order.parentorder.service;
 
+import com.atzuche.order.parentorder.entity.OrderRecordEntity;
 import com.atzuche.order.parentorder.mapper.OrderRecordMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,8 @@ public class OrderRecordService{
     @Autowired
     private OrderRecordMapper orderRecordMapper;
 
+    public Integer save(OrderRecordEntity orderRecordEntity){
+        return orderRecordMapper.insertSelective(orderRecordEntity);
+    }
 
 }
