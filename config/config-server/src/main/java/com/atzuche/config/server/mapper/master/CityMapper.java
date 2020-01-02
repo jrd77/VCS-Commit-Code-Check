3fba8c8eaf8d95d07f1ee57773353ab10a424bb6
Andy Liang <andysjtu@163.com>
2020-01-02 10:17:38 +0800

    add city config

---
+++ config/config-server/src/main/java/com/atzuche/config/server/mapper/master/CityMapper.java
@@ -0,0 +1,16 @@
+package com.atzuche.config.server.mapper.master;
+
+import com.atzuche.config.common.entity.CityEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:lianglin.sjtu@gmail.com">AndySjtu</a>
+ * @date 2020/1/2 10:05 上午
+ **/
+@Mapper
+public interface CityMapper {
+
+    List<CityEntity> findAll();
+}
