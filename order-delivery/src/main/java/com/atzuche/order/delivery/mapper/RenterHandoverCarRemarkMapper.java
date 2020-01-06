27d01cb13b5ffc94be31fba9b20490037bb1d1f9
胡春林 <445777020@qq.com>
2020-01-06 13:09:54 +0800

    交接车/配送服务

--- order-delivery/src/main/java/com/atzuche/order/delivery/mapper/RenterHandoverCarRemarkMapper.java
+++ order-delivery/src/main/java/com/atzuche/order/delivery/mapper/RenterHandoverCarRemarkMapper.java
@@ -1,7 +1,11 @@
 package com.atzuche.order.delivery.mapper;
 
+import com.atzuche.order.delivery.entity.OwnerHandoverCarInfoEntity;
 import com.atzuche.order.delivery.entity.RenterHandoverCarRemarkEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 租客端交车备注表
@@ -21,5 +25,12 @@ public interface RenterHandoverCarRemarkMapper{
     int updateByPrimaryKey(RenterHandoverCarRemarkEntity record);
     
     int updateByPrimaryKeySelective(RenterHandoverCarRemarkEntity record);
+
+    /**
+     * 根据订单号和类型查询
+     * @param orderNo
+     * @return
+     */
+    List<RenterHandoverCarRemarkEntity> selectObjectByOrderNo(@Param("orderNo") String orderNo);
 
 }
