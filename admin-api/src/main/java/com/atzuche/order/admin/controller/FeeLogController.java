4dfacb82fdcc7e63d9ba79be71d0eec1cd4770e6
jun.liu <0plm9okn8ijb>
2020-01-06 20:02:25 +0800

    车辆详情接口文档

--- admin-api/src/main/java/com/atzuche/order/admin/controller/FeeLogController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/FeeLogController.java
@@ -3,7 +3,8 @@ package com.atzuche.order.admin.controller;
 import com.atzuche.order.admin.vo.req.fee.FeeLogReqVO;
 import com.atzuche.order.admin.vo.resp.car.CarDepositReturnDetailResVO;
 import com.atzuche.order.admin.vo.resp.fee.FeeLogRespVO;
-import com.autoyol.car.api.model.vo.ResponseObject;
+
+import com.autoyol.commons.web.ResponseData;
 import com.autoyol.doc.annotation.AutoDocMethod;
 import com.autoyol.doc.annotation.AutoDocVersion;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,7 @@ public class FeeLogController {
 
     @AutoDocMethod(description = "费用操作日志", value = "费用操作日志", response = FeeLogRespVO.class)
     @PostMapping(value = "/fee/log/list")
-    public ResponseObject<?> getFeeLog(@RequestBody FeeLogReqVO feeLogReqVO){
+    public ResponseData <?> getFeeLog(@RequestBody FeeLogReqVO feeLogReqVO){
 
         return null;
     }
