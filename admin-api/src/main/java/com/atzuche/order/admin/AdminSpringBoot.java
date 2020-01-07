c1ae95c1926d8d83d1a6dfabcba368c932877280
胡春林 <445777020@qq.com>
2020-01-07 20:21:36 +0800

    交接车/配送服务

--- admin-api/src/main/java/com/atzuche/order/admin/AdminSpringBoot.java
+++ admin-api/src/main/java/com/atzuche/order/admin/AdminSpringBoot.java
@@ -1,5 +1,6 @@
 package com.atzuche.order.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,7 +12,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
  * @date 2020/1/2 1:57 下午
  **/
 @SpringBootApplication(scanBasePackages = {"com.atzuche.order.admin","com.atzuche.order.delivery"})
-
+@MapperScan({"com.atzuche.order.delivery"})
 @EnableFeignClients({"com.autoyol"})
 public class AdminSpringBoot extends SpringBootServletInitializer {
 
