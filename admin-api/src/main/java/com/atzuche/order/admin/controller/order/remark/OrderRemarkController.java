2c3e608819f75b772fb479c43232191688e09a88
qincai.lin <qincai.lin@atzuche.cn>
2020-01-08 16:25:06 +0800

    备注编辑接口

--- admin-api/src/main/java/com/atzuche/order/admin/controller/order/remark/OrderRemarkController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/order/remark/OrderRemarkController.java
@@ -1,11 +1,14 @@
 package com.atzuche.order.admin.controller.order.remark;
 
+import com.atzuche.order.admin.controller.order.insurance.OrderInsuranceController;
 import com.atzuche.order.admin.service.remark.OrderRemarkService;
 import com.atzuche.order.admin.vo.req.remark.*;
 import com.atzuche.order.admin.vo.resp.remark.*;
 import com.autoyol.commons.web.ResponseData;
 import com.autoyol.doc.annotation.AutoDocMethod;
 import com.autoyol.doc.annotation.AutoDocVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +18,22 @@ import org.springframework.web.bind.annotation.*;
 @AutoDocVersion(version = "订单备注接口文档")
 public class OrderRemarkController {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderRemarkController.class);
+
     @Autowired
     OrderRemarkService orderRemarkService;
 
 	@AutoDocMethod(description = "备注总览", value = "备注总览", response = OrderRemarkOverviewListResponseVO.class)
 	@GetMapping("/overview")
 	public ResponseData<OrderRemarkOverviewListResponseVO> getOverview(@RequestBody OrderRemarkRequestVO orderRemarkRequestVO, BindingResult bindingResult) {
-		return ResponseData.success(orderRemarkService.getOrderRemarkOverview(orderRemarkRequestVO));
+	    try{
+            logger.info("获取备注总览");
+            return ResponseData.success(orderRemarkService.getOrderRemarkOverview(orderRemarkRequestVO));
+        } catch (Exception e) {
+            logger.info("获取备注总览{}",e);
+        }
+		return null;
 	}
 
     @AutoDocMethod(description = "备注查询列表", value = "备注查询列表", response = OrderRemarkPageListResponseVO.class)
@@ -47,12 +59,14 @@ public class OrderRemarkController {
     @AutoDocMethod(description = "删除备注", value = "删除备注", response = ResponseData.class)
     @DeleteMapping("/delete")
     public ResponseData<ResponseData> delete(@RequestBody OrderRemarkDeleteRequestVO orderRemarkDeleteRequestVO, BindingResult bindingResult) {
+        orderRemarkService.updateRemarkById(null);
         return ResponseData.success(null);
     }
 
     @AutoDocMethod(description = "编辑备注", value = "编辑备注", response = ResponseData.class)
     @RequestMapping(value = "/update", method = RequestMethod.PUT)
     public ResponseData<ResponseData> update(@RequestBody OrderRemarkUpdateRequestVO orderRemarkUpdateRequestVO, BindingResult bindingResult) {
+        //orderRemarkService.updateRemarkById(orderRemarkUpdateRequestVO);
         return ResponseData.success(null);
     }
 
