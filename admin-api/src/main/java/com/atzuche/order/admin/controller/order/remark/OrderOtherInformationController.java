60c3b4a4f1d5155a88a143078e244925adec83a9
qincai.lin <qincai.lin@atzuche.cn>
2020-01-09 19:40:30 +0800

    其他信息接口

--- admin-api/src/main/java/com/atzuche/order/admin/controller/order/remark/OrderOtherInformationController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/order/remark/OrderOtherInformationController.java
@@ -3,6 +3,7 @@ package com.atzuche.order.admin.controller.order.remark;
 import com.atzuche.order.admin.vo.req.remark.OrderGetReturnCarRemarkRequestVO;
 import com.atzuche.order.admin.vo.req.remark.OrderRentCityRequestVO;
 import com.atzuche.order.admin.vo.req.remark.OrderRiskStatusRequestVO;
+import com.atzuche.order.admin.vo.resp.remark.OrderOtherInformationResponseVO;
 import com.autoyol.commons.web.ResponseData;
 import com.autoyol.doc.annotation.AutoDocMethod;
 import com.autoyol.doc.annotation.AutoDocVersion;
@@ -31,5 +32,16 @@ public class OrderOtherInformationController {
     public ResponseData<ResponseData> updateGetReturnCarRemark(@RequestBody OrderGetReturnCarRemarkRequestVO orderGetReturnCarRemarkRequestVO, BindingResult bindingResult) {
         return ResponseData.success(null);
     }
+
+    @AutoDocMethod(description = "获取其他备注信息(包括租车城市,风控事故，取送车备注)", value = "获取其他备注信息(包括租车城市,风控事故，取送车备注)", response = OrderOtherInformationResponseVO.class)
+    @GetMapping("/detail")
+    public ResponseData<ResponseData> getOtherRemarkInformation( OrderRentCityRequestVO OrderRentCityRequestVO, BindingResult bindingResult) {
+        OrderOtherInformationResponseVO orderOtherInformationResponseVO = new OrderOtherInformationResponseVO();
+        orderOtherInformationResponseVO.setRentCity("上海");
+        orderOtherInformationResponseVO.setRiskAccidentStatus("1");
+        orderOtherInformationResponseVO.setRemarkContent("12点送车到点");
+        return ResponseData.success(null);
+    }
+
 
 }
