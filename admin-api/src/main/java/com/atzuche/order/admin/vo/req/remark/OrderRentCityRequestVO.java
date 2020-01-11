259ee561164be9ec471e8f2ffb4d7b9d239c3f38
qincai.lin <qincai.lin@atzuche.cn>
2020-01-11 15:37:17 +0800

    添加验证信息

--- admin-api/src/main/java/com/atzuche/order/admin/vo/req/remark/OrderRentCityRequestVO.java
+++ admin-api/src/main/java/com/atzuche/order/admin/vo/req/remark/OrderRentCityRequestVO.java
@@ -3,12 +3,14 @@ package com.atzuche.order.admin.vo.req.remark;
 import com.autoyol.doc.annotation.AutoDocProperty;
 import lombok.Data;
 import lombok.ToString;
+import org.hibernate.validator.constraints.NotBlank;
 
 @Data
 @ToString
 public class OrderRentCityRequestVO {
 
     @AutoDocProperty(value = "订单号")
+    @NotBlank(message = "订单号不能为空")
     private String orderNo;
 
     @AutoDocProperty(value = "用车城市")
