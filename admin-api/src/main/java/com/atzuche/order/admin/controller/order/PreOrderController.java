6232b3f53c2d7bca8f036e9bcd119ea122b9c8d1
Andy Liang <andysjtu@163.com>
2020-01-11 17:48:54 +0800

    complete the admin preOrder

--- admin-api/src/main/java/com/atzuche/order/admin/controller/order/PreOrderController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/order/PreOrderController.java
@@ -3,6 +3,8 @@ package com.atzuche.order.admin.controller.order;
 import com.atzuche.order.admin.vo.req.order.PreOrderAdminRequestVO;
 import com.atzuche.order.admin.vo.resp.order.PreOrderAdminResponseVO;
 import com.atzuche.order.car.CarProxyService;
+import com.atzuche.order.commons.entity.dto.RenterGoodsDetailDTO;
+import com.atzuche.order.commons.entity.dto.RenterGoodsPriceDetailDTO;
 import com.autoyol.commons.web.ErrorCode;
 import com.autoyol.commons.web.ResponseData;
 import com.atzuche.order.mem.MemProxyService;
@@ -12,6 +14,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 下单前管理后台页面展示
@@ -34,6 +41,9 @@ public class PreOrderController {
             return new ResponseData<>(ErrorCode.INPUT_ERROR.getCode(), ErrorCode.INPUT_ERROR.getText());
         }
 
+        PreOrderAdminResponseVO responseVO = new PreOrderAdminResponseVO();
+
+
         String memNo = request.getMemNo();
         if(StringUtils.trimToNull(memNo)==null){
             String mobile = request.getMobile();
@@ -44,17 +54,42 @@ public class PreOrderController {
             memNo = memProxyService.getMemNoByMoile(mobile).toString();
         }
 
+        responseVO.setMemNo(memNo);
+
+
         CarProxyService.CarDetailReqVO carDetailReqVO = new CarProxyService.CarDetailReqVO();
         carDetailReqVO.setAddrIndex(0);
         carDetailReqVO.setCarNo(request.getCarNo());
         carDetailReqVO.setUseSpecialPrice(true);
 
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        LocalDateTime rentTime = LocalDateTime.parse(request.getRentTime(),dateTimeFormatter);
 
+        LocalDateTime revertTime = LocalDateTime.parse(request.getRevertTime(),dateTimeFormatter);
+        carDetailReqVO.setRentTime(rentTime);
+        carDetailReqVO.setRevertTime(revertTime);
 
+        RenterGoodsDetailDTO renterGoodsDetailDTO = carProxyService.getRenterGoodsDetail(carDetailReqVO);
 
+        responseVO.setCarPlatNo(renterGoodsDetailDTO.getCarPlateNum());
+        List<RenterGoodsPriceDetailDTO> renterGoodsPriceDetailDTOList = renterGoodsDetailDTO.getRenterGoodsPriceDetailDTOList();
 
-        //FIXME:temp
-        return null;
+        List<PreOrderAdminResponseVO.CarDayPrice> carDayPrices = new ArrayList<>();
+
+        for(RenterGoodsPriceDetailDTO dto:renterGoodsPriceDetailDTOList){
+            PreOrderAdminResponseVO.CarDayPrice carDayPrice = new PreOrderAdminResponseVO.CarDayPrice();
+            carDayPrice.setDay(dto.getRevertTime().format(DateTimeFormatter.BASIC_ISO_DATE));
+            carDayPrice.setPrice(dto.getCarUnitPrice().toString());
+            carDayPrice.setDesc("工作日");
+        }
+
+        responseVO.setCarSpecialDayPrices(carDayPrices);
+
+        //TODO:
+        responseVO.setTotalWallet(0);
+        responseVO.setTotalAutoCoin(0);
+
+        return ResponseData.success(responseVO);
 
     }
 }
