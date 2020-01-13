db3cd8b2417ddd1a69aaa4678ccf7fa1a9737775
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-01-13 14:47:52 +0800

    调整feign接口使用

--- util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/orderDetailDto/OrderHistoryReqDTO.java
+++ util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/orderDetailDto/OrderHistoryReqDTO.java
@@ -1,8 +1,24 @@
 package com.atzuche.order.commons.entity.orderDetailDto;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 public class OrderHistoryReqDTO {
-
+    /**
+     * 主订单号
+     */
+    @NotBlank(message = "主订单号不能为空")
+    @NotNull(message = "主订单号不能为空")
+    private String orderNo;
+    /**
+     * 租客子订单号
+     */
+    private String renterOrderNo;
+    /**
+     * 车主子订单号
+     */
+    private String ownerOrderNo;
 }
