0ebd8658a4871988192a245a4b29fd9a310ac5a1
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-01-13 15:11:35 +0800

    调整feign接口使用

--- util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/orderDetailDto/OrderHistoryRespDTO.java
+++ util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/orderDetailDto/OrderHistoryRespDTO.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 @Data
 public class OrderHistoryRespDTO {
-
-    List<RenterOrderDTO> renterOrderDTOList;
-
-    List<OwnerOrderDTO> ownerOrderDTOLIst;
+    public OrderDTO orderDTO;
+    /**
+     * 租客历史订单(包含生效的租客订单)
+     */
+    public List<RenterOrderDTO> renterOrderDTOHistoryList;
+    /**
+     * 车主历史订单（包含生效的车主订单）
+     */
+    public List<OwnerOrderDTO> ownerOrderDTOHistoryLIst;
 
 }
