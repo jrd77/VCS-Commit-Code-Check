dbc37af9395d4fa18ccad9c524984445d299f750
Andy Liang <andysjtu@163.com>
2020-01-13 21:25:40 +0800

    complete the car/renter/owner info

--- admin-api/src/main/java/com/atzuche/order/admin/controller/CarOwnerInfoController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/CarOwnerInfoController.java
@@ -2,12 +2,24 @@ package com.atzuche.order.admin.controller;
 
 import com.atzuche.order.admin.vo.req.car.CarMemNoReqVO;
 import com.atzuche.order.admin.vo.resp.car.CarOwnerInfoRespVO;
+import com.atzuche.order.admin.vo.resp.car.RenterInfoRespVO;
+import com.atzuche.order.car.CarProxyService;
+import com.atzuche.order.commons.entity.orderDetailDto.OrderDetailReqDTO;
+import com.atzuche.order.commons.entity.orderDetailDto.OrderDetailRespDTO;
+import com.atzuche.order.mem.MemProxyService;
+import com.atzuche.order.mem.dto.OrderRenterInfoDTO;
+import com.atzuche.order.open.service.FeignOrderDetailService;
 import com.autoyol.commons.web.ResponseData;
 import com.autoyol.doc.annotation.AutoDocMethod;
 import com.autoyol.doc.annotation.AutoDocVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -16,16 +28,41 @@ import javax.validation.Valid;
 @RestController
 @AutoDocVersion(version = "订单详细信息 - 查看车主信息接口文档")
 public class CarOwnerInfoController {
+    private final static Logger logger = LoggerFactory.getLogger(CarOwnerInfoController.class);
+    
 
-    /**
-     * 老后台参考:
-     * com.autoyolConsole.controller.TransController.detail(String, String, HttpServletRequest)
-     * /autoyolConsole/src/main/webapp/WEB-INF/view/trans/detail.jsp
-     */
+    @Autowired
+    private MemProxyService memProxyService;
+
+    @Autowired
+    private CarProxyService carProxyService;
+
+    @Autowired
+    private FeignOrderDetailService feignOrderDetailService;
+
     @AutoDocMethod(description = "查看车主信息接口信息", value = "查看车主信息接口信息", response = CarOwnerInfoRespVO.class)
     @GetMapping(value = "/info")
-    public ResponseData<?> getCarOwnerInfo(@Valid CarMemNoReqVO reqVo, BindingResult bindingResult) {
-        return null;
+    public ResponseData<?> getCarOwnerInfo(@RequestParam("orderNo")String orderNo,@RequestParam("memNo")String memNo) {
+        logger.info("reqVo is order={},memNo={}",orderNo,memNo);
+        OrderRenterInfoDTO orderRenterInfoDTO =  memProxyService.getRenterInfoByMemNo(memNo);
+        CarOwnerInfoRespVO respVO = new CarOwnerInfoRespVO();
+        respVO.setRealName(orderRenterInfoDTO.getRealName());
+        respVO.setCity(orderRenterInfoDTO.getCity());
+        respVO.setDriLicRecordNo(orderRenterInfoDTO.getDriLicRecordNo());
+        respVO.setEmail(orderRenterInfoDTO.getEmail());
+        respVO.setGender(orderRenterInfoDTO.getGender());
+        respVO.setIdNo(orderRenterInfoDTO.getIdNo());
+        respVO.setOwnerNo(orderRenterInfoDTO.getMemNo());
+        respVO.setProvince(orderRenterInfoDTO.getProvince());
+
+        OrderDetailReqDTO reqDTO = new OrderDetailReqDTO();
+        reqDTO.setOrderNo("45697331100299");
+        ResponseData<OrderDetailRespDTO> result = feignOrderDetailService.getOrderDetail(reqDTO);
+
+        logger.info("feign is {}",result.getData());
+
+        //TODO:车主等级
+        return ResponseData.success(respVO);
     }
 
 }
--- admin-api/src/main/java/com/atzuche/order/admin/controller/RenterInfoController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/RenterInfoController.java
@@ -8,11 +8,14 @@ import com.autoyol.commons.web.ErrorCode;
 import com.autoyol.commons.web.ResponseData;
 import com.autoyol.doc.annotation.AutoDocMethod;
 import com.autoyol.doc.annotation.AutoDocVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -21,20 +24,21 @@ import javax.validation.Valid;
 @RestController
 @AutoDocVersion(version = "订单详细信息 - 查看租客信息接口文档")
 public class RenterInfoController {
+    
+    private final static Logger logger = LoggerFactory.getLogger(RenterInfoController.class);
+    
 
     @Autowired
     private MemProxyService memProxyService;
 
     @AutoDocMethod(description = "获取租客信息接口响应信息", value = "获取租客信息接口响应信息", response = RenterInfoRespVO.class)
     @GetMapping(value = "/info")
-    public ResponseData<RenterInfoRespVO> getRenterInfo(@Valid CarOrderReqVO reqVo, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()){
-            return new ResponseData<>(ErrorCode.INPUT_ERROR.getCode(), ErrorCode.INPUT_ERROR.getText());
-        }
-        OrderRenterInfoDTO orderRenterInfoDTO =  memProxyService.getRenterInfoByMemNo(reqVo.getMemNo());
+    public ResponseData<RenterInfoRespVO> getRenterInfo(@RequestParam(value = "orderNo",required = true) String orderNo,@RequestParam(value = "memNo",required = true) String memNo) {
+        logger.info("reqVo is {},{}",orderNo,memNo);
+        OrderRenterInfoDTO orderRenterInfoDTO =  memProxyService.getRenterInfoByMemNo(memNo);
         RenterInfoRespVO respVO = new RenterInfoRespVO();
         BeanUtils.copyProperties(orderRenterInfoDTO,respVO);
-
+        //TODO:下单地址
         return ResponseData.success(respVO);
     }
 }
