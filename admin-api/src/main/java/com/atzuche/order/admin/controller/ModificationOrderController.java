36723a1a5b2d333dfc4420c4591ea5fa8d66dca9
huangjing <754599404@qq.com>
2020-01-14 15:40:10 +0800

    提交去掉memNo

--- admin-api/src/main/java/com/atzuche/order/admin/controller/ModificationOrderController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/ModificationOrderController.java
@@ -3,7 +3,6 @@ package com.atzuche.order.admin.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
