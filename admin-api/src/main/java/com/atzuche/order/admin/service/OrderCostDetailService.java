c74bf0654ec85cb8bc3e8a8d4565bb669c9b43be
huangjing <754599404@qq.com>
2020-01-15 20:38:03 +0800

    提交异常处理

--- admin-api/src/main/java/com/atzuche/order/admin/service/OrderCostDetailService.java
+++ admin-api/src/main/java/com/atzuche/order/admin/service/OrderCostDetailService.java
@@ -7,6 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.atzuche.order.admin.vo.req.cost.RenterCostReqVO;
+import com.atzuche.order.admin.vo.resp.order.cost.detail.OrderRenterFineAmtDetailResVO;
+
 /**
  * @author jing.huang
  *
@@ -14,6 +17,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderCostDetailService {
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+	public OrderRenterFineAmtDetailResVO findfineAmtListByOrderNo(RenterCostReqVO renterCostReqVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 }
