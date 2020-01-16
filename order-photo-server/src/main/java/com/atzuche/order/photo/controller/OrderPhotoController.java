1890cd3fa987dfebd245b13b96a3d570fb27addb
qincai.lin <qincai.lin@atzuche.cn>
2020-01-16 14:41:13 +0800

    上传订单照片添加异常

--- order-photo-server/src/main/java/com/atzuche/order/photo/controller/OrderPhotoController.java
+++ order-photo-server/src/main/java/com/atzuche/order/photo/controller/OrderPhotoController.java
@@ -3,8 +3,8 @@ package com.atzuche.order.photo.controller;
 
 import com.atzuche.order.photo.common.AdminUserUtil;
 import com.atzuche.order.photo.dto.OrderPhotoDTO;
-import com.atzuche.order.photo.entity.OrderPhotoEntity;
 import com.atzuche.order.photo.enums.UserTypeEnum;
+import com.atzuche.order.photo.exception.OrderPhotoException;
 import com.atzuche.order.photo.service.OrderPhotoService;
 import com.atzuche.order.photo.util.oss.PicUtils;
 import com.atzuche.order.photo.vo.req.OrderDeleteRequestVO;
@@ -13,6 +13,7 @@ import com.atzuche.order.photo.vo.req.OrderUpdateRequestVO;
 import com.atzuche.order.photo.vo.req.OrderUploadRequestVO;
 import com.atzuche.order.photo.vo.resp.OrderPhotoResponseVO;
 import com.atzuche.order.photo.vo.resp.OrderViolationPhotoResponseVO;
+import com.autoyol.commons.web.ErrorCode;
 import com.autoyol.commons.web.ResponseData;
 import com.autoyol.doc.annotation.AutoDocMethod;
 import com.autoyol.doc.annotation.AutoDocVersion;
@@ -59,6 +60,7 @@ public class OrderPhotoController{
 	@AutoDocMethod(description = "订单照片列表", value = "订单照片列表", response = OrderPhotoResponseVO.class)
 	@GetMapping("/list")
 	public ResponseData list(@Valid OrderRequestVO orderRequestVO, BindingResult bindingResult){
+        validateParameter(bindingResult);
 		String orderNo = orderRequestVO.getOrderNo();
 	    List<OrderPhotoDTO> getCarList =orderPhotoService.queryGetSrvCarList(orderNo, UserTypeEnum.RENTER.getType());
 	    List<OrderPhotoDTO> srvCarList =orderPhotoService.queryGetSrvCarList(orderNo, UserTypeEnum.OWNER.getType());
@@ -86,15 +88,15 @@ public class OrderPhotoController{
 	 */
 	@PostMapping("upload")
 	@AutoDocMethod(description = "上传订单照片", value = "上传订单照片", response = ResponseData.class)
-    public ResponseData uploadOrderPhoto(@RequestParam("picFiles") MultipartFile[] files, @Valid OrderUploadRequestVO orderUploadRequestVO) throws Exception {
-		String operator =  AdminUserUtil.getAdminUser().getAuthName();
+    public ResponseData uploadOrderPhoto(@RequestParam("picFiles") MultipartFile[] files, @Valid OrderUploadRequestVO orderUploadRequestVO, BindingResult bindingResult) throws Exception {
+        validateParameter(bindingResult);
+	    String operator =  AdminUserUtil.getAdminUser().getAuthName();
 
 		String msg = null;
 			if(files !=null && files.length >0){
 				List<MultipartFile> data = new ArrayList<MultipartFile>();
 				if(files.length>10 ){
-					msg ="一次上传图片最多不能超过10张,请重新选择！";
-					return null;
+                    return ResponseData.createErrorCodeResponse(ErrorCode.FAILED.getCode(), "一次上传图片最多不能超过10张,请重新选择！");
 				}
 				for(MultipartFile multifile :files){
 					if(multifile !=null && multifile.getSize() != 0){
@@ -103,19 +105,18 @@ public class OrderPhotoController{
 							int fileSize = inputTmp.available();
 							logger.info("Upload File size:{}",fileSize);
 							if(fileSize > PicUtils.FILE_LIMIT_SIZE){
-								msg ="上传文件不能大于3M";
-
+                                return ResponseData.createErrorCodeResponse(ErrorCode.FAILED.getCode(), "上传文件不能大于3M");
 							}
 							
 							String picType = PicUtils.getPicType((InputStream)inputTmp);
 							if(!PicUtils.TYPE_JPG.equals(picType)&&!PicUtils.TYPE_PNG.equals(picType)){
 								logger.error("不支持该图片格式！");
-								msg= "上传失败,请上传 jpg 或  png 格式的图片";
+                                return ResponseData.createErrorCodeResponse(ErrorCode.FAILED.getCode(), "上传失败,请上传 jpg 或  png 格式的图片");
 							}
 							data.add(multifile);
 						}catch (IOException e) {
 							logger.error("上传违章处理图片报IO流异常："+e.getMessage());
-
+                            return ResponseData.createErrorCodeResponse(ErrorCode.FAILED.getCode(), "上传违章处理图片报IO流异常");
 						}
 					}
 					
@@ -140,7 +141,8 @@ public class OrderPhotoController{
 	 */
 	@DeleteMapping("/delete")
 	@AutoDocMethod(description = "删除订单照片", value = "删除订单照片", response = ResponseData.class)
-	public ResponseData delOrderPhoto(@RequestBody OrderDeleteRequestVO orderDeleteRequestVO){
+	public ResponseData delOrderPhoto(@RequestBody OrderDeleteRequestVO orderDeleteRequestVO, BindingResult bindingResult){
+        validateParameter(bindingResult);
 		orderPhotoService.delOrderPhoto(orderDeleteRequestVO.getPhotoType(),orderDeleteRequestVO.getPhotoId());
 		return ResponseData.success();
 	}
@@ -162,31 +164,42 @@ public class OrderPhotoController{
 	 */
 	@PostMapping("update")
 	@AutoDocMethod(description = "变更订单照片", value = "变更订单照片", response = ResponseData.class)
-	public ResponseData updateOrderPhoto(@RequestParam("picFile") MultipartFile multipartFile, @Valid OrderUpdateRequestVO orderUpdateRequestVO) throws Exception {
-		String operator =  AdminUserUtil.getAdminUser().getAuthName();
-		String msg = null;
+	public ResponseData updateOrderPhoto(@RequestParam("picFile") MultipartFile multipartFile, @Valid OrderUpdateRequestVO orderUpdateRequestVO, BindingResult bindingResult) throws Exception {
+        validateParameter(bindingResult);
+	    String operator =  AdminUserUtil.getAdminUser().getAuthName();
+
 		if(multipartFile !=null){
 			try {
 				InputStream inputTmp = multipartFile.getInputStream();
 				int fileSize = inputTmp.available();
 				logger.info("Upload File size:{}",fileSize);
 				if(fileSize > PicUtils.FILE_LIMIT_SIZE){
-					msg ="上传文件不能大于3M";
+                    return ResponseData.createErrorCodeResponse(ErrorCode.FAILED.getCode(), "上传文件不能大于3M");
 				}
 
 				String picType = PicUtils.getPicType((InputStream)inputTmp);
 				if(!PicUtils.TYPE_JPG.equals(picType)&&!PicUtils.TYPE_PNG.equals(picType)){
 					logger.error("不支持该图片格式！");
-					msg= "上传失败,请上传 jpg 或  png 格式的图片";
+                    return ResponseData.createErrorCodeResponse(ErrorCode.FAILED.getCode(), "上传失败,请上传 jpg 或  png 格式的图片");
 				}
 				orderPhotoService.uploadOrderPhoto(multipartFile, orderUpdateRequestVO, operator);
 			}catch (IOException e) {
 				logger.error("上传违章处理图片报IO流异常："+e.getMessage());
+                return ResponseData.createErrorCodeResponse(ErrorCode.FAILED.getCode(), "上传违章处理图片报IO流异常");
 
 			}
 		}
-
 		return ResponseData.success();
 	}
+
+    /**
+     * 验证参数
+     * @param bindingResult
+     */
+    private void validateParameter(BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new OrderPhotoException(ErrorCode.PARAMETER_ERROR.getCode(),bindingResult.getFieldError().getDefaultMessage());
+        }
+    }
 
 }
