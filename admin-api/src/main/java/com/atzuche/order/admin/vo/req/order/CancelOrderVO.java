ac8448bf5550a8289033ab1a228abc4847118438
Andy Liang <andysjtu@163.com>
2020-01-16 11:32:18 +0800

    fix

--- admin-api/src/main/java/com/atzuche/order/admin/vo/req/order/CancelOrderVO.java
+++ admin-api/src/main/java/com/atzuche/order/admin/vo/req/order/CancelOrderVO.java
@@ -10,7 +10,7 @@ import org.hibernate.validator.constraints.NotBlank;
  * @date 2020/1/7 11:27 上午
  **/
 @Data
-public class CancelOrderVO extends BaseVO {
+public class CancelOrderVO {
     @AutoDocProperty(value = "订单号")
     @NotBlank(message = "订单号不能为空")
     private String orderNo;
