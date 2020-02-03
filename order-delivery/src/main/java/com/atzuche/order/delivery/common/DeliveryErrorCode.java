190307da7bb81821a7fdd092e4c85352e0b27d2b
胡春林 <445777020@qq.com>
2020-02-03 14:43:43 +0800

    交接车/配送服务

--- order-delivery/src/main/java/com/atzuche/order/delivery/common/DeliveryErrorCode.java
+++ order-delivery/src/main/java/com/atzuche/order/delivery/common/DeliveryErrorCode.java
@@ -9,6 +9,7 @@ public enum  DeliveryErrorCode{
     SEND_REN_YUN_HTTP_ERROR("300001","请求仁云接口出错"),
     DELIVERY_PARAMS_ERROR("300002","接受参数出错"),
     DELIVERY_MOUDLE_ERROR("300003","配送模块业务出错"),
+    NO_DELIVERY_INFO("300004","该订单没有相关的配送信息"),
     PIC_KEY_PARAM_ERROR("300072","图片相对路径+图片名称不能为空"),
     ;
 
