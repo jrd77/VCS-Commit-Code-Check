7c311abb4f8abcda848f6ae8804ee62d830ff5b5
Andy Liang <andysjtu@163.com>
2020-02-13 17:07:46 +0800

    添加单前优惠券列表接口

--- admin-api/src/main/java/com/atzuche/order/admin/service/AdminOrderService.java
+++ admin-api/src/main/java/com/atzuche/order/admin/service/AdminOrderService.java
@@ -5,6 +5,7 @@ import com.atzuche.order.admin.vo.req.order.AdminModifyOrderReqVO;
 import com.atzuche.order.admin.vo.req.order.CancelOrderByPlatVO;
 import com.atzuche.order.admin.vo.req.order.CancelOrderVO;
 import com.atzuche.order.admin.vo.req.order.OrderModifyConfirmReqVO;
+import com.atzuche.order.admin.vo.resp.MemAvailableCouponVO;
 import com.atzuche.order.admin.vo.resp.order.AdminModifyOrderFeeCompareVO;
 import com.atzuche.order.admin.vo.resp.order.AdminModifyOrderFeeVO;
 import com.atzuche.order.commons.CatConstants;
@@ -12,10 +13,10 @@ import com.atzuche.order.commons.entity.dto.OrderTransferRecordDTO;
 import com.atzuche.order.commons.entity.orderDetailDto.OrderDetailReqDTO;
 import com.atzuche.order.commons.entity.orderDetailDto.OrderDetailRespDTO;
 import com.atzuche.order.commons.exceptions.RemoteCallException;
-import com.atzuche.order.commons.vo.req.AdminOrderCancelReqVO;
-import com.atzuche.order.commons.vo.req.CancelOrderReqVO;
-import com.atzuche.order.commons.vo.req.ModifyApplyHandleReq;
-import com.atzuche.order.commons.vo.req.ModifyOrderReqVO;
+import com.atzuche.order.commons.vo.req.*;
+import com.atzuche.order.commons.vo.res.NormalOrderCostCalculateResVO;
+import com.atzuche.order.commons.vo.res.order.*;
+import com.atzuche.order.open.service.FeignOrderCostService;
 import com.atzuche.order.open.service.FeignOrderDetailService;
 import com.atzuche.order.open.service.FeignOrderModifyService;
 import com.atzuche.order.open.service.FeignOrderUpdateService;
@@ -32,6 +33,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -44,6 +46,73 @@ public class AdminOrderService {
 
     @Autowired
     private FeignOrderDetailService feignOrderDetailService;
+
+    @Autowired
+    private FeignOrderCostService feignOrderCostService;
+
+
+    public MemAvailableCouponVO getPreOrderCouponList(NormalOrderCostCalculateReqVO reqVO){
+        Transaction t = Cat.newTransaction(CatConstants.FEIGN_CALL, "订单CoreAPI");
+        MemAvailableCouponVO memAvailableCouponVO = new MemAvailableCouponVO();
+        try{
+            Cat.logEvent(CatConstants.FEIGN_METHOD,"feignOrderCostService.cancelOrder");
+            log.info("Feign 开始下单前费用试算,reqVO={}", JSON.toJSONString(reqVO));
+            Cat.logEvent(CatConstants.FEIGN_PARAM,JSON.toJSONString(reqVO));
+            ResponseData<NormalOrderCostCalculateResVO> responseObject = feignOrderCostService.submitOrderBeforeCostCalculate(reqVO);
+            log.info("response=[{}]",JSON.toJSONString(responseObject));
+            Cat.logEvent(CatConstants.FEIGN_RESULT,JSON.toJSONString(responseObject));
+            checkResponse(responseObject);
+            t.setStatus(Transaction.SUCCESS);
+            NormalOrderCostCalculateResVO resVO = responseObject.getData();
+            ReductionVO reductionVO = resVO.getReduction();
+            if(reductionVO!=null){
+                CouponReductionVO couponReductionVO = reductionVO.getCouponReduction();
+                CarOwnerCouponReductionVO carOwnerCouponReductionVO = reductionVO.getCarOwnerCouponReduction();
+                if(couponReductionVO!=null){
+                    List<DisCouponMemInfoVO> discoupons = couponReductionVO.getDiscoupons();
+                    if(discoupons!=null){
+                        memAvailableCouponVO.setPlatCouponList(extractNotGetCarCouponList(discoupons));
+                        memAvailableCouponVO.setGetCarCouponList(extractGetCarCouponList(discoupons));
+                    }
+
+                }
+                if(carOwnerCouponReductionVO!=null){
+                    List<CarOwnerCouponDetailVO>   availableCouponList = carOwnerCouponReductionVO.getAvailableCouponList();
+                    memAvailableCouponVO.setCarOwnerCouponDetailVOList(availableCouponList);
+                }
+            }
+            return memAvailableCouponVO;
+
+        }catch (Exception e){
+            log.error("调用远程开始下单前费用试算失败,param={}",reqVO,e);
+            Cat.logError("Feign 开始下单前费用试算失败",e);
+            t.setStatus(e);
+            throw e;
+        }finally {
+            t.complete();
+        }
+    }
+
+    private List<DisCouponMemInfoVO> extractGetCarCouponList(List<DisCouponMemInfoVO> discoupons) {
+        List<DisCouponMemInfoVO> list = new ArrayList<>();
+        for(DisCouponMemInfoVO vo:discoupons){
+            if("8".equals(vo.getPlatformCouponType())){
+                list.add(vo);
+            }
+        }
+        return list;
+    }
+
+    private List<DisCouponMemInfoVO> extractNotGetCarCouponList(List<DisCouponMemInfoVO> discoupons) {
+        List<DisCouponMemInfoVO> list = new ArrayList<>();
+        for(DisCouponMemInfoVO vo:discoupons){
+            if(!"8".equals(vo.getPlatformCouponType())){
+                list.add(vo);
+            }
+        }
+        return list;
+    }
+
 
     public ResponseData cancelOrder(CancelOrderVO cancelOrderVO) {
         CancelOrderReqVO cancelOrderReqVO = new CancelOrderReqVO();
--- order-openapi/src/main/java/com/atzuche/order/open/service/FeignOrderCostService.java
+++ order-openapi/src/main/java/com/atzuche/order/open/service/FeignOrderCostService.java
@@ -3,7 +3,10 @@
  */
 package com.atzuche.order.open.service;
 
+import com.atzuche.order.commons.vo.req.NormalOrderCostCalculateReqVO;
+import com.atzuche.order.commons.vo.res.NormalOrderCostCalculateResVO;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -11,6 +14,8 @@ import com.atzuche.order.commons.vo.req.OrderCostReqVO;
 import com.atzuche.order.commons.vo.res.OrderOwnerCostResVO;
 import com.atzuche.order.commons.vo.res.OrderRenterCostResVO;
 import com.autoyol.commons.web.ResponseData;
+
+import javax.validation.Valid;
 
 /**
  * @author jing.huang
@@ -26,5 +31,13 @@ public interface FeignOrderCostService {
 	
 	@PostMapping("/order/cost/owner/get")
 	public ResponseData<OrderOwnerCostResVO> orderCostOwnerGet(@RequestBody OrderCostReqVO req);
+
+	/**
+	 * 下单前费用计算
+	 * @param reqVO
+	 * @return
+	 */
+	@PostMapping("/normal/pre/cost/calculate")
+	public ResponseData<NormalOrderCostCalculateResVO> submitOrderBeforeCostCalculate(@Valid @RequestBody NormalOrderCostCalculateReqVO reqVO);
 	
 }
