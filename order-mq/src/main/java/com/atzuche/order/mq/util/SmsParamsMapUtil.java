bf04ac9bb1efe2e0b79982729855f156e3c33965
胡春林 <445777020@qq.com>
2020-02-15 03:33:46 +0800

    交接车/配送服务

--- order-mq/src/main/java/com/atzuche/order/mq/util/SmsParamsMapUtil.java
+++ order-mq/src/main/java/com/atzuche/order/mq/util/SmsParamsMapUtil.java
@@ -18,23 +18,24 @@ public class SmsParamsMapUtil {
      * 组装基础数据
      *
      * @param orderNo    订单号
-     * @param renterFlag 租客textCode
-     * @param ownerFlag  车主textCode
+     * @param renterTextCodeFlag 租客textCode
+     * @param ownerTextCodeFlag  车主textCode
      * @param paramsMap  其他参数
      * @return
      */
-    public static Map getParamsMap(String orderNo, String renterFlag, String ownerFlag, Map paramsMap) {
+    public static Map getParamsMap(String orderNo, String renterTextCodeFlag, String ownerTextCodeFlag, Map paramsMap) {
 
         Map map = Maps.newHashMap();
         if (StringUtils.isBlank(orderNo)) {
             log.info("没有传入订单号，orderNo:[{}]", orderNo);
             return Maps.newHashMap();
         }
-        if (StringUtils.isNotBlank(renterFlag)) {
-            map.put("renterFlag", renterFlag);
+        map.put("orderNo", orderNo);
+        if (StringUtils.isNotBlank(renterTextCodeFlag)) {
+            map.put("renterFlag", renterTextCodeFlag);
         }
-        if (StringUtils.isNotBlank(ownerFlag)) {
-            map.put("ownerFlag", ownerFlag);
+        if (StringUtils.isNotBlank(ownerTextCodeFlag)) {
+            map.put("ownerFlag", ownerTextCodeFlag);
         }
         if (!CollectionUtils.isEmpty(paramsMap)) {
             map.putAll(paramsMap);
