fe15ed98684d2aaeddaba47fcb150f87195d41f2
胡春林 <445777020@qq.com>
2020-02-15 03:35:03 +0800

    交接车/配送服务

--- order-core-api/src/main/java/com/atzuche/order/coreapi/listener/sms/OrderActionEventListener.java
+++ order-core-api/src/main/java/com/atzuche/order/coreapi/listener/sms/OrderActionEventListener.java
@@ -1,10 +1,21 @@
 package com.atzuche.order.coreapi.listener.sms;
 
 import com.alibaba.fastjson.JSONObject;
+import com.atzuche.order.commons.entity.dto.RenterGoodsDetailDTO;
+import com.atzuche.order.commons.entity.dto.RenterMemberDTO;
 import com.atzuche.order.mq.common.base.OrderMessage;
 import com.atzuche.order.mq.util.MQSendPlatformSmsService;
+import com.atzuche.order.owner.commodity.service.OwnerGoodsService;
+import com.atzuche.order.owner.mem.service.OwnerMemberService;
+import com.atzuche.order.ownercost.entity.OwnerOrderEntity;
+import com.atzuche.order.ownercost.service.OwnerOrderService;
+import com.atzuche.order.rentercommodity.service.RenterGoodsService;
+import com.atzuche.order.rentermem.service.RenterMemberService;
+import com.atzuche.order.renterorder.entity.RenterOrderEntity;
+import com.atzuche.order.renterorder.service.RenterOrderService;
 import com.dianping.cat.Cat;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -12,6 +23,9 @@ import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Map;
 
 /**
  * @author 胡春林
@@ -23,6 +37,18 @@ public class OrderActionEventListener {
 
     @Autowired
     MQSendPlatformSmsService sendPlatformSmsService;
+    @Autowired
+    RenterMemberService renterMemberService;
+    @Autowired
+    OwnerMemberService ownerMemberService;
+    @Autowired
+    RenterGoodsService renterGoodsService;
+    @Autowired
+    OwnerGoodsService ownerGoodsService;
+    @Autowired
+    RenterOrderService renterOrderService;
+    @Autowired
+    OwnerOrderService ownerOrderService;
 
     @RabbitListener(bindings = {@QueueBinding(value = @Queue(value = "order_action_03", durable = "true"),
             exchange = @Exchange(value = "auto-order-action", durable = "true", type = "topic"), key = "action.order.create4.#")
@@ -32,8 +58,26 @@ public class OrderActionEventListener {
         OrderMessage orderMessage = JSONObject.parseObject(message.getBody(),OrderMessage.class);
         log.info("新订单动作总事件监听,入参orderMessage:[{}]", orderMessage.toString());
         try {
-//            sendPlatformSmsService.orderPaySms(orderMessage.getContext(), orderMessage.getPhone(), "大事件備注",null);
-//            log.info("新订单action事件成功发送短信,----------->>>>>>内容：{},手机号:{}",orderMessage.getContext(),orderMessage.getPhone());
+            Map smsParamsMap = orderMessage.getMap();
+            if(CollectionUtils.isEmpty(smsParamsMap))
+            {
+                log.info("没有短信需要发送--->>>>orderMessage:[{}]",orderMessage.toString());
+                return;
+            }
+            if(!smsParamsMap.containsKey("orderNo"))
+            {
+                log.info("缺少短信需要发送的订单号参数--->>>>orderMessage:[{}]",orderMessage.toString());
+                return;
+            }
+            String orderNo = String.valueOf(smsParamsMap.get("orderNo"));
+            //根据订单号查询相关参数数据
+            if(StringUtils.isNotBlank(String.valueOf(smsParamsMap.get("renterFlag"))))
+            {
+//                RenterOrderEntity renterOrderEntity = renterOrderService.getRenterOrderByOrderNoAndIsEffective(orderNo);
+//                RenterMemberDTO renterMemberDTO = renterMemberService.selectrenterMemberByRenterOrderNo(renterOrderEntity.getRenterOrderNo(),false);
+//                RenterGoodsDetailDTO renterGoodsDetailDTO = renterGoodsService.getRenterGoodsDetail(renterOrderEntity.getRenterOrderNo(),false);
+                return;
+            }
         } catch (Exception e) {
             log.info("新订单动作总事件监听发生异常,msg：[{}]",e.getMessage());
             Cat.logError("新订单动作总事件监听发生异常",e);
