3b6dcd2ced4847c33e68b5700fc90465252109e4
闫海豹 <haibao.yan@atzuche.com>
2020-02-17 20:21:03 +0800

    bug 修改

--- admin-api/src/main/java/com/atzuche/order/admin/controller/AdminOrderCostController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/AdminOrderCostController.java
@@ -6,15 +6,22 @@ package com.atzuche.order.admin.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.atzuche.order.admin.service.AdminDeliveryCarService;
+import com.atzuche.order.commons.enums.cashcode.OwnerCashCodeEnum;
+import com.atzuche.order.commons.enums.cashcode.RenterCashCodeEnum;
+import com.atzuche.order.delivery.vo.delivery.rep.DeliveryCarVO;
+import com.atzuche.order.delivery.vo.delivery.rep.OwnerGetAndReturnCarDTO;
+import com.atzuche.order.delivery.vo.delivery.rep.RenterGetAndReturnCarDTO;
+import com.atzuche.order.delivery.vo.delivery.req.DeliveryCarRepVO;
+import com.atzuche.order.ownercost.entity.OwnerOrderIncrementDetailEntity;
+import com.atzuche.order.ownercost.service.OwnerOrderIncrementDetailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.atzuche.order.admin.service.OrderCostService;
 import com.atzuche.order.admin.vo.req.cost.OwnerCostReqVO;
@@ -26,6 +33,9 @@ import com.autoyol.commons.web.ResponseData;
 import com.autoyol.doc.annotation.AutoDocMethod;
 import com.autoyol.doc.annotation.AutoDocVersion;
 import com.dianping.cat.Cat;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author jing.huang
@@ -51,6 +61,15 @@ public class AdminOrderCostController {
         try {
         	
         	OrderRenterCostResVO resp = orderCostService.calculateRenterOrderCost(renterCostReqVO);
+            // 计算油量和超里程费用
+            DeliveryCarVO deliveryCarRepVO = getDeliveryCarVO(renterCostReqVO.getOrderNo());
+            if(Objects.nonNull(deliveryCarRepVO) && Objects.nonNull(deliveryCarRepVO.getOwnerGetAndReturnCarDTO())){
+                RenterGetAndReturnCarDTO renterGetAndReturnCarDTO = deliveryCarRepVO.getRenterGetAndReturnCarDTO();
+                if(Objects.nonNull(renterGetAndReturnCarDTO)){
+                    resp.setOilAmt(renterGetAndReturnCarDTO.getOilDifferenceCrash());
+                    resp.setBeyondMileAmt(renterGetAndReturnCarDTO.getOverKNCrash());
+                }
+            }
         	return ResponseData.success(resp);
 		} catch (Exception e) {
 			Cat.logError("calculateRenterOrderCost exception params="+renterCostReqVO.toString(),e);
@@ -59,8 +78,12 @@ public class AdminOrderCostController {
 		}
 		
 	}
-	
-	
+
+    @Autowired
+    private OwnerOrderIncrementDetailService ownerOrderIncrementDetailService;
+    @Autowired
+    private AdminDeliveryCarService deliveryCarInfoService;
+
 	@AutoDocMethod(description = "计算车主子订单费用", value = "计算车主子订单费用", response = OrderOwnerCostResVO.class)
 	@RequestMapping(value="calculateOwnerOrderCost",method = RequestMethod.POST)
 	public ResponseData calculateOwnerOrderCost(@RequestBody @Validated OwnerCostReqVO ownerCostReqVO, HttpServletRequest request, HttpServletResponse response,BindingResult bindingResult) {
@@ -72,6 +95,30 @@ public class AdminOrderCostController {
         try {
         	
         	OrderOwnerCostResVO resp = orderCostService.calculateOwnerOrderCost(ownerCostReqVO);
+            // 计算 Gps 和平台服务费
+        	List<OwnerOrderIncrementDetailEntity> list=  ownerOrderIncrementDetailService.listOwnerOrderIncrementDetail(ownerCostReqVO.getOrderNo(),ownerCostReqVO.getOwnerOrderNo());
+            if(!CollectionUtils.isEmpty(list)){
+                int gpsAmt = list.stream().filter(obj ->{
+                    return OwnerCashCodeEnum.GPS_SERVICE_AMT.getCashNo().equals(obj.getCostCode());
+                }).mapToInt(OwnerOrderIncrementDetailEntity::getTotalAmount).sum();
+                int serviceAmt = list.stream().filter(obj ->{
+                    return OwnerCashCodeEnum.SERVICE_CHARGE.getCashNo().equals(obj.getCostCode());
+                }).mapToInt(OwnerOrderIncrementDetailEntity::getTotalAmount).sum();
+                resp.setGpsAmt(String.valueOf(gpsAmt));
+                resp.setPlatformSrvFeeAmt(String.valueOf(serviceAmt));
+            }else{
+                resp.setGpsAmt("0");
+                resp.setPlatformSrvFeeAmt("0");
+            }
+            // 计算油量和超里程费用
+            DeliveryCarVO deliveryCarRepVO = getDeliveryCarVO(ownerCostReqVO.getOrderNo());
+            if(Objects.nonNull(deliveryCarRepVO) && Objects.nonNull(deliveryCarRepVO.getOwnerGetAndReturnCarDTO())){
+                OwnerGetAndReturnCarDTO ownerGetAndReturnCarDTO = deliveryCarRepVO.getOwnerGetAndReturnCarDTO();
+                if(Objects.nonNull(ownerGetAndReturnCarDTO)){
+                    resp.setOilAmt(ownerGetAndReturnCarDTO.getOilDifferenceCrash());
+                    resp.setBeyondMileAmt(ownerGetAndReturnCarDTO.getOverKNCrash());
+                }
+            }
         	logger.info("resp = " + resp.toString());
         	return ResponseData.success(resp);
 		} catch (Exception e) {
@@ -81,6 +128,12 @@ public class AdminOrderCostController {
 		}
 		
 	}
-	
-	
+
+	private DeliveryCarVO getDeliveryCarVO(String orderNo){
+        DeliveryCarRepVO deliveryCarDTO = new DeliveryCarRepVO();
+        deliveryCarDTO.setOrderNo(orderNo);
+        DeliveryCarVO deliveryCarRepVO = deliveryCarInfoService.findDeliveryListByOrderNo(deliveryCarDTO);
+        return deliveryCarRepVO;
+    }
+
 }
