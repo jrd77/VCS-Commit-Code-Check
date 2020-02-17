a2f8f902c1de7d537ec1fca77382c40ab449606c
Andy Liang <andysjtu@163.com>
2020-02-17 17:03:05 +0800

    添加日志

--- admin-api/src/main/java/com/atzuche/order/admin/controller/AdminOrderSubmitController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/AdminOrderSubmitController.java
@@ -1,12 +1,16 @@
 package com.atzuche.order.admin.controller;
 
+import com.atzuche.order.admin.constant.AdminOpTypeEnum;
 import com.atzuche.order.admin.service.OrderSubmitService;
+import com.atzuche.order.admin.service.log.AdminLogService;
 import com.atzuche.order.admin.vo.req.orderSubmit.AdminTransReqVO;
 import com.atzuche.order.commons.vo.res.OrderResVO;
 import com.autoyol.commons.web.ErrorCode;
 import com.autoyol.commons.web.ResponseData;
 import com.autoyol.doc.annotation.AutoDocMethod;
 import com.autoyol.doc.annotation.AutoDocVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,14 +28,27 @@ public class AdminOrderSubmitController {
     @Autowired
     private OrderSubmitService orderSubmitService;
 
+    @Autowired
+    private AdminLogService adminLogService;
+    
+    private final static Logger logger = LoggerFactory.getLogger(AdminOrderSubmitController.class);
+    
+
+
     @AutoDocMethod(description = "下单", value = "下单", response = OrderResVO.class)
     @PostMapping("/submit")
     public ResponseData<OrderResVO> submit(@Valid @RequestBody AdminTransReqVO adminOrderReqVO, BindingResult bindingResult, HttpServletRequest request) throws Exception {
         if (bindingResult.hasErrors()) {
             return new ResponseData<>(ErrorCode.INPUT_ERROR.getCode(), ErrorCode.INPUT_ERROR.getText());
         }
-        ResponseData<OrderResVO> responseData = orderSubmitService.submit(adminOrderReqVO,request);
-        return responseData;
+        OrderResVO orderResVO = orderSubmitService.submit(adminOrderReqVO,request);
+        try{
+            adminLogService.insertLog(AdminOpTypeEnum.SUBMIT_ORDER,orderResVO.getOrderNo(),adminOrderReqVO.toString());
+        }catch (Exception e){
+            logger.warn("后台日志记录错误",e);
+        }
+
+        return ResponseData.success(orderResVO);
     }
 
 
