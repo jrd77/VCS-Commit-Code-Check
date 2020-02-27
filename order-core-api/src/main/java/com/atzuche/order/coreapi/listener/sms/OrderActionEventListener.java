07147bbca2c443ad6115725ff7c5f69801887639
胡春林 <445777020@qq.com>
2020-02-28 05:18:47 +0800

    交接车/配送服务

--- order-core-api/src/main/java/com/atzuche/order/coreapi/listener/sms/OrderActionEventListener.java
+++ order-core-api/src/main/java/com/atzuche/order/coreapi/listener/sms/OrderActionEventListener.java
@@ -1,6 +1,7 @@
 package com.atzuche.order.coreapi.listener.sms;
 
 import com.alibaba.fastjson.JSONObject;
+import com.atzuche.order.coreapi.listener.push.OrderSendMessageFactory;
 import com.atzuche.order.mq.common.base.OrderMessage;
 import com.dianping.cat.Cat;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
@@ -21,6 +23,9 @@ import java.util.Map;
 @Component
 @Slf4j
 public class OrderActionEventListener extends SMSOrderBaseEventService{
+
+    @Autowired
+    OrderSendMessageFactory orderSendMessageFactory;
 
     @RabbitListener(bindings = {@QueueBinding(value = @Queue(value = "order_action_01", durable = "true"),
             exchange = @Exchange(value = "auto-order-action", durable = "true", type = "topic"), key = "action.#")
@@ -40,6 +45,17 @@ public class OrderActionEventListener extends SMSOrderBaseEventService{
                 return;
             }
             sendShortMessage(smsParamsMap);
+            Map pushParamsMap = orderMessage.getPushMap();
+            if (CollectionUtils.isEmpty(pushParamsMap)) {
+                log.info("没有push/通知需要发送--->>>>orderMessage:[{}]", orderMessage.toString());
+                return;
+            }
+            if (!pushParamsMap.containsKey("orderNo")) {
+                log.info("缺少push/通知需要发送的订单号参数--->>>>orderMessage:[{}]", orderMessage.toString());
+                return;
+            }
+            orderSendMessageFactory.sendPushMessage(pushParamsMap);
+
         } catch (Exception e) {
             log.info("新订单动作总事件监听发生异常,msg：[{}]", e);
             Cat.logError("新订单动作总事件监听发生异常", e);
