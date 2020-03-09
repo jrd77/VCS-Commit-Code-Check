e3c64cbe1f27b7c085f34c5adee4be7b9348a30a
胡春林 <445777020@qq.com>
2020-03-09 16:28:19 +0800

    交接车/配送服务

--- order-core-api/src/main/java/com/atzuche/order/coreapi/task/NoPayRentCarHalfHoursTask.java
+++ order-core-api/src/main/java/com/atzuche/order/coreapi/task/NoPayRentCarHalfHoursTask.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -49,7 +48,7 @@ public class NoPayRentCarHalfHoursTask extends IJobHandler {
             for (OrderDTO violateBO : orderNos) {
                 OrderStatusEntity orderStatusEntity = orderStatusService.getByOrderNo(violateBO.getOrderNo());
                 if (orderStatusEntity.getRentCarPayStatus().intValue() == 0) {
-                    if (SMSTaskDateTimeUtils.getDateLatterCompareNowScoend(LocalDateTime.now(), 60) == 30) {
+                    if (SMSTaskDateTimeUtils.getDateLatterCompareNowScoend(violateBO.getReqTime(), 60) == 30) {
                         remindPayIllegalCrashService.sendNoPayCarShortMessageData(violateBO.getOrderNo());
                     }
                 }
