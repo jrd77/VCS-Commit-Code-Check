39328725175ab4843420c5a86bd448b2ab141e91
胡春林 <445777020@qq.com>
2020-03-20 18:34:07 +0800

    交接车/配送服务

--- wz-service/src/main/java/com/atzuche/violation/controller/ViolationInfoController.java
+++ wz-service/src/main/java/com/atzuche/violation/controller/ViolationInfoController.java
@@ -60,7 +60,7 @@ public class ViolationInfoController {
             CatLogRecord.successLog("违章管理列表成功", "console/order/violation/list", violationReqVO);
             return ResponseData.success(pageModel);
         } catch (Exception e) {
-            logger.error("违章管理列表异常:{}", e);
+            logger.error("违章管理列表异常",e);
             CatLogRecord.failLog("违章管理列表异常", "console/order/violation/list", violationReqVO, e);
             throw new ViolationManageException(ErrorCode.SYS_ERROR.getCode(), ErrorCode.SYS_ERROR.getText());
         }
@@ -87,7 +87,7 @@ public class ViolationInfoController {
             CatLogRecord.successLog("违章明细管理列表成功", "console/order/violation/detailList", violationDetailReqVO);
             return ResponseData.success(renterOrderWzDetailResVOS);
         } catch (Exception e) {
-            logger.error("违章明细管理列表异常:{}", e);
+            logger.error("违章明细管理列表异常", e);
             CatLogRecord.failLog("违章明细管理列表异常", "console/order/violation/detailList", violationDetailReqVO, e);
             throw new ViolationManageException(ErrorCode.SYS_ERROR.getCode(), ErrorCode.SYS_ERROR.getText());
         }
@@ -105,7 +105,7 @@ public class ViolationInfoController {
         try {
             violationInfoService.export(violationReqVO, response);
         } catch (Exception e) {
-            logger.error("收益审核列表导出excel异常:{}", e.getMessage());
+            logger.error("收益审核列表导出excel异常", e);
         }
     }
 
