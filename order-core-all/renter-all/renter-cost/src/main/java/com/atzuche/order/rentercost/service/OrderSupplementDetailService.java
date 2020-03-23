8c3657920153e8e6147f5b4e66f38d58166d6356
seazhao <haishui.zhao@atzuche.cn>
2020-03-23 10:54:29 +0800

    #xxx

--- order-core-all/renter-all/renter-cost/src/main/java/com/atzuche/order/rentercost/service/OrderSupplementDetailService.java
+++ order-core-all/renter-all/renter-cost/src/main/java/com/atzuche/order/rentercost/service/OrderSupplementDetailService.java
@@ -57,13 +57,13 @@ public class OrderSupplementDetailService{
     	//操作状态:0,待提交 1,已生效 2,已失效 3,已撤回
     	entity.setOpStatus(1);
     	//操作类型:1,修改订单 2,车管家录入 3,租车押金结算 4,违章押金结算 5,手动添加
-    	entity.setOpType(5);
+    	entity.setOpType(1);
     	entity.setOrderNo(orderNo);
     	//支付状态:0.无需支付 1.未支付 2.已取消 3.已支付 4.支付中，5.支付失败 10.租车押金结算抵扣  20.违章押金结算抵扣
     	entity.setPayFlag(1);
     	entity.setRemark("动态添加记录");
     	//补付类型:1,系统创建 2,手动创建
-    	entity.setSupplementType(2);
+    	entity.setSupplementType(1);
     	entity.setTitle(cashCode.getTxt());
     	entity.setCreateOp("系统动态创建");
     	entity.setCreateTime(new Date());
