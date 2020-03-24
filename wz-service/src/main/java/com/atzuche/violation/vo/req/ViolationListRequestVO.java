35bc2d14e8bda97f0fbe5e7af879fb18dd58d575
qincai.lin <qincai.lin@atzuche.cn>
2020-03-24 17:41:52 +0800

    添加验证

--- wz-service/src/main/java/com/atzuche/violation/vo/req/ViolationListRequestVO.java
+++ wz-service/src/main/java/com/atzuche/violation/vo/req/ViolationListRequestVO.java
@@ -3,6 +3,7 @@ package com.atzuche.violation.vo.req;
 import com.autoyol.doc.annotation.AutoDocProperty;
 import lombok.Data;
 import lombok.ToString;
+import org.hibernate.validator.constraints.NotBlank;
 
 /**
  * Created by qincai.lin on 2020/3/16.
@@ -12,6 +13,7 @@ import lombok.ToString;
 public class ViolationListRequestVO {
 
     @AutoDocProperty(value = "订单号")
+    @NotBlank(message = "主订单号不能为空")
     private String orderNo; // 订单号
 
 }
