9c2c424354b25583a3d3042f3c5dd86ae8472a43
pengcheng.fu <pengcheng.fu@atzuche.com>
2020-03-25 14:24:32 +0800

    临时 commit

---
+++ order-core-api/src/main/java/com/atzuche/order/coreapi/entity/dto/CheckCarDispatchResDTO.java
@@ -0,0 +1,23 @@
+package com.atzuche.order.coreapi.entity.dto;
+
+import lombok.Data;
+
+/**
+ * 订单是否进调度
+ *
+ * @author pengcheng.fu
+ * @date 2020/3/2514:17
+ */
+@Data
+public class CheckCarDispatchResDTO {
+
+    /**
+     * 是否进入调度
+     */
+    private Boolean isDispatch;
+
+    /**
+     * 不进调度的原因
+     */
+    private String noDispatchReason;
+}
