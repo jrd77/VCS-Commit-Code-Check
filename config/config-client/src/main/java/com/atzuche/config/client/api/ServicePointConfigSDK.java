5db4ddec1f52ac204b46ffc63a6108515db92d42
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-04-01 17:16:39 +0800

    修改配置

--- config/config-client/src/main/java/com/atzuche/config/client/api/ServicePointConfigSDK.java
+++ config/config-client/src/main/java/com/atzuche/config/client/api/ServicePointConfigSDK.java
@@ -4,8 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.atzuche.config.common.api.ConfigContext;
 import com.atzuche.config.common.api.ConfigItemDTO;
-import com.atzuche.config.common.api.ConfigNotFoundException;
-import com.atzuche.config.common.entity.CityEntity;
 import com.atzuche.config.common.entity.ServicePointEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,7 +42,8 @@ public class ServicePointConfigSDK {
             }
         }
         logger.error("configNot found for address={},lat={},lon={}",address,lat,lon);
-        throw new ConfigNotFoundException("ConfigNotFound:address="+address+",lat="+lat+",lon="+lon);
+        //throw new ConfigNotFoundException("ConfigNotFound:address="+address+",lat="+lat+",lon="+lon);*/
+        return null;
     }
 
 
