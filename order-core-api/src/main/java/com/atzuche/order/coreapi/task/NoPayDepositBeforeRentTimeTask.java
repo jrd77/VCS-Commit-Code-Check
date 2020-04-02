fd7cc0ba5091efc965ea1dfdeb923ca16b534555
胡春林 <445777020@qq.com>
2020-04-02 11:08:58 +0800

    交接车/配送服务

--- order-core-api/src/main/java/com/atzuche/order/coreapi/task/NoPayDepositBeforeRentTimeTask.java
+++ order-core-api/src/main/java/com/atzuche/order/coreapi/task/NoPayDepositBeforeRentTimeTask.java
@@ -56,7 +56,7 @@ public class NoPayDepositBeforeRentTimeTask extends IJobHandler {
             }
             for (OrderDTO violateBO : orderNos) {
                 OrderStatusEntity orderStatusEntity = orderStatusService.getByOrderNo(violateBO.getOrderNo());
-                logger.info("当前订单状态数据：orderStatusEntity:[{}],预计取车时间[{}]", orderStatusEntity.getStatus().toString(), violateBO.getExpRentTime());
+                logger.info("当前订单状态数据：orderStatusEntity:[{}],预计取车时间[{}]", orderStatusEntity.getOrderNo()+"---"+orderStatusEntity.getStatus().toString(), violateBO.getExpRentTime());
                 if (orderStatusEntity.getStatus().intValue() < 8 && (orderStatusEntity.getDepositPayStatus().intValue() == 0 || orderStatusEntity.getWzPayStatus().intValue() == 0)) {
                     if (LocalDateTime.now().isBefore(violateBO.getExpRentTime()) && LocalDateTime.now().plusMinutes(5).isAfter(violateBO.getExpRentTime())) {
                         remindPayIllegalCrashService.sendNoPayIllegalDepositShortMessageData(violateBO.getOrderNo());
