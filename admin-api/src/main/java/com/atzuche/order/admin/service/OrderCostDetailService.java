1cb08d58ba0bd71546111f4ae96614ea35e8d435
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-04-02 17:17:17 +0800

    会员权益列表

--- admin-api/src/main/java/com/atzuche/order/admin/service/OrderCostDetailService.java
+++ admin-api/src/main/java/com/atzuche/order/admin/service/OrderCostDetailService.java
@@ -149,7 +149,7 @@ public class OrderCostDetailService {
         	if(renterDepositDetailEntity.getOriginalDepositAmt()!=null && renterDepositDetailEntity.getReductionDepositAmt()!=null) {
         		reductionAfterRentDepost = String.valueOf(renterDepositDetailEntity.getOriginalDepositAmt().intValue() - renterDepositDetailEntity.getReductionDepositAmt().intValue());
         	}
-        	yearCoefficient = String.valueOf(renterDepositDetailEntity.getSuggestTotal());
+        	yearCoefficient = String.valueOf(renterDepositDetailEntity.getNewCarCoefficient());
         	brandCoefficient = String.valueOf(renterDepositDetailEntity.getCarSpecialCoefficient());
         	
         }
@@ -179,6 +179,7 @@ public class OrderCostDetailService {
 			List<RenterMemberRightDTO> renterMemberRightDTOList) {
 		
 		int totalReductionOrderRatio = 0;
+		int totalReductionItemGetRatio = 0;
 		for (RenterMemberRightDTO renterMemberRightDTO : renterMemberRightDTOList) {
 			//任务
 			if(renterMemberRightDTO.getRightType().intValue() == RightTypeEnum.TASK.getCode().intValue()) {
@@ -190,12 +191,14 @@ public class OrderCostDetailService {
 				///
 				reductTaskList.add(task);
 
-                totalReductionOrderRatio += renterMemberRightDTO.getRightValue()==null?0:Integer.valueOf(renterMemberRightDTO.getRightValue());
+                totalReductionOrderRatio += task.getReductionOrderRatio()==null?0:Integer.valueOf(task.getReductionOrderRatio());
+                totalReductionItemGetRatio += task.getReductionItemGetRatio()==null?0:Integer.valueOf(task.getReductionItemGetRatio());
 			}
 		}
         String reductionOrderRatio = String.valueOf(totalReductionOrderRatio>=70?70:totalReductionOrderRatio);
+		String reductionItemGetRatio = String.valueOf(totalReductionItemGetRatio>=70?70:totalReductionItemGetRatio);
         ReductionTaskResVO task = new ReductionTaskResVO();
-        //task.setReductionItemGetRatio(renterMemberRightDTO.getRightValue());
+        task.setReductionItemGetRatio(reductionItemGetRatio);
         task.setReductionItemName("总计");
         task.setReductionItemRule("最高减免比例70%（总计超过70%，按70%计算）");
         task.setReductionOrderRatio(reductionOrderRatio);
