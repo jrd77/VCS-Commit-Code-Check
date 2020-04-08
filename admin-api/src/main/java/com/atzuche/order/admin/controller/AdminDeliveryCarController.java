850fd2f919d11719d62b6602fd616dbf49ec8792
胡春林 <445777020@qq.com>
2020-04-08 20:03:55 +0800

    交接车/配送服务

--- admin-api/src/main/java/com/atzuche/order/admin/controller/AdminDeliveryCarController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/AdminDeliveryCarController.java
@@ -1,10 +1,8 @@
 package com.atzuche.order.admin.controller;
 
 import com.atzuche.order.admin.service.AdminDeliveryCarService;
-import com.atzuche.order.admin.service.log.AdminLogService;
 import com.atzuche.order.delivery.exception.DeliveryOrderException;
 import com.atzuche.order.delivery.vo.delivery.rep.DeliveryCarVO;
-import com.atzuche.order.delivery.vo.delivery.req.CarConditionPhotoUploadVO;
 import com.atzuche.order.delivery.vo.delivery.req.DeliveryCarRepVO;
 import com.autoyol.commons.web.ErrorCode;
 import com.autoyol.commons.web.ResponseData;
@@ -33,9 +31,6 @@ public class AdminDeliveryCarController extends BaseController {
 
     @Autowired
     private AdminDeliveryCarService deliveryCarInfoService;
-
-    @Autowired
-    private AdminLogService adminLogService;
 
     /**
      * 获取配送信息
