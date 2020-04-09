70b39b9d78923ec58e4972a49ee70630e4a88403
seazhao <haishui.zhao@atzuche.cn>
2020-04-09 15:13:26 +0800

    #长租

--- order-core-all/renter-all/renter-order/src/main/java/com/atzuche/order/renterorder/service/OwnerCouponLongService.java
+++ order-core-all/renter-all/renter-order/src/main/java/com/atzuche/order/renterorder/service/OwnerCouponLongService.java
@@ -2,6 +2,9 @@ package com.atzuche.order.renterorder.service;
 
 import com.atzuche.order.renterorder.entity.OwnerCouponLongEntity;
 import com.atzuche.order.renterorder.mapper.OwnerCouponLongMapper;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +24,30 @@ public class OwnerCouponLongService{
 
     public OwnerCouponLongEntity getByRenterOrderNo(String renterOrderNo) {
         return ownerCouponLongMapper.getByRenterOrderNo(renterOrderNo);
+    }
+    
+    /**
+     * 保存长租折扣信息
+     * @param ownerCouponLongEntity
+     * @return Integer
+     */
+    public Integer saveOwnerCouponLong(OwnerCouponLongEntity ownerCouponLongEntity) {
+    	if (ownerCouponLongEntity == null) {
+    		return 0;
+    	}
+    	return ownerCouponLongMapper.insertSelective(ownerCouponLongEntity);
+    }
+    
+    /**
+     * 批量保存长租折扣信息
+     * @param list
+     */
+    public void saveOwnerCouponLongBatch(List<OwnerCouponLongEntity> list) {
+    	if (list == null || list.isEmpty()) {
+    		return;
+    	}
+    	for (OwnerCouponLongEntity record:list) {
+    		saveOwnerCouponLong(record);
+    	}
     }
 }
