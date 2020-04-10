f266196e6003d6ce0387c3e98ebe6433336b5f02
胡春林 <445777020@qq.com>
2020-04-10 15:55:22 +0800

    交接车/配送服务

--- order-core-api/src/main/java/com/atzuche/order/coreapi/task/RemindPushPayDepositTask.java
+++ order-core-api/src/main/java/com/atzuche/order/coreapi/task/RemindPushPayDepositTask.java
@@ -1,14 +1,29 @@
 package com.atzuche.order.coreapi.task;
 
+import com.alibaba.fastjson.JSONObject;
 import com.atzuche.order.commons.CatConstants;
+import com.atzuche.order.commons.entity.dto.OwnerGoodsDetailDTO;
+import com.atzuche.order.commons.entity.dto.RenterGoodsDetailDTO;
 import com.atzuche.order.commons.entity.orderDetailDto.OrderDTO;
+import com.atzuche.order.commons.enums.CarOwnerTypeEnum;
+import com.atzuche.order.coreapi.entity.SmsMsgSendLogEntity;
+import com.atzuche.order.coreapi.enums.ShortMessageCodeEnum;
+import com.atzuche.order.coreapi.listener.push.OrderSendMessageFactory;
+import com.atzuche.order.coreapi.mapper.SmsMsgSendLogMapper;
 import com.atzuche.order.coreapi.service.RemindPayIllegalCrashService;
 import com.atzuche.order.coreapi.utils.SMSTaskDateTimeUtils;
+import com.atzuche.order.mq.enums.PushMessageTypeEnum;
+import com.atzuche.order.mq.util.SmsParamsMapUtil;
+import com.atzuche.order.ownercost.entity.OwnerOrderEntity;
 import com.atzuche.order.parentorder.entity.OrderStatusEntity;
 import com.atzuche.order.parentorder.service.OrderStatusService;
+import com.atzuche.order.rentercommodity.service.RenterGoodsService;
+import com.atzuche.order.renterorder.entity.RenterOrderEntity;
+import com.atzuche.order.renterorder.service.RenterOrderService;
 import com.autoyol.commons.utils.DateUtil;
 import com.dianping.cat.Cat;
 import com.dianping.cat.message.Transaction;
+import com.google.common.collect.Maps;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
@@ -20,7 +35,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author 胡春林
@@ -34,6 +52,15 @@ public class RemindPushPayDepositTask extends IJobHandler {
     OrderStatusService orderStatusService;
     @Resource
     private RemindPayIllegalCrashService remindPayIllegalCrashService;
+    @Autowired
+    OrderSendMessageFactory orderSendMessageFactory;
+    @Autowired
+    RenterOrderService renterOrderService;
+    @Autowired
+    RenterGoodsService renterGoodsService;
+    @Autowired
+    @Resource
+    SmsMsgSendLogMapper smsMsgSendLogMapper;
     @Override
     public ReturnT<String> execute(String s) throws Exception {
         Transaction t = Cat.getProducer().newTransaction(CatConstants.XXL_JOB_CALL, "您还未支付预定车辆的押金，请在X小时内完成支付。否则该订单将被取消 定时任务");
@@ -49,7 +76,29 @@ public class RemindPushPayDepositTask extends IJobHandler {
             for (OrderDTO violateBO : orderNos) {
                 OrderStatusEntity orderStatusEntity = orderStatusService.getByOrderNo(violateBO.getOrderNo());
                 if (orderStatusEntity.getStatus().intValue() != 0 && orderStatusEntity.getWzPayStatus().intValue() == 0 && orderStatusEntity.getStatus().intValue() < 8) {
-                    //没有支付违章押金
+                    SmsMsgSendLogEntity smsMsgSendLogEntity = smsMsgSendLogMapper.selectByOrderNoAndMemNo(violateBO.getOrderNo(), violateBO.getMemNoRenter(), ShortMessageCodeEnum.PUSH_DEPOSETID_COST_4_HOURS.getValue().intValue());
+                    if (Objects.isNull(smsMsgSendLogEntity) || smsMsgSendLogEntity.getStatus().intValue() == 0) {
+                        //没有支付违章押金 是否是凹凸自營車輛
+                        boolean result = CarOwnerTypeEnum.isAuToByCode(getCarOwnerType(violateBO.getOrderNo()));
+                        Map paraMap = Maps.newHashMap();
+                        if (result) {
+                            paraMap.put("leftHours", 2);
+                        } else {
+                            paraMap.put("leftHours", 4);
+                        }
+                        Map pushMap = SmsParamsMapUtil.getParamsMap(violateBO.getOrderNo(), PushMessageTypeEnum.RENTER_NO_PAY_ILLEGAL.getValue(), null, paraMap);
+                        orderSendMessageFactory.sendPushMessage(pushMap);
+                        SmsMsgSendLogEntity smsMsgSendLog = new SmsMsgSendLogEntity();
+                        smsMsgSendLog.setMemNo(violateBO.getMemNoRenter());
+                        smsMsgSendLog.setOrderNo(violateBO.getOrderNo());
+                        smsMsgSendLog.setMsgType(ShortMessageCodeEnum.PRE_CAR_COST_4_HOURS.getValue().intValue());
+                        smsMsgSendLog.setSendParams(JSONObject.toJSONString(paraMap));
+                        smsMsgSendLog.setSendTime(LocalDateTime.now());
+                        smsMsgSendLog.setStatus(1);
+                        int count = smsMsgSendLog.getSendTotal() == null ? 0 : smsMsgSendLog.getSendTotal();
+                        smsMsgSendLog.setSendTotal(count + 1);
+                        smsMsgSendLogMapper.insertSelective(smsMsgSendLog);
+                    }
                 }
             }
             logger.info("结束执行 您还未支付预定车辆的押金，请在X小时内完成支付。否则该订单将被取消 ");
@@ -66,4 +115,17 @@ public class RemindPushPayDepositTask extends IJobHandler {
             }
         }
     }
+
+    /**
+     * 获取车主车辆类型
+     * @param orderNo
+     * @return
+     */
+    public Integer getCarOwnerType(String orderNo) {
+        RenterOrderEntity renterOrderEntity = renterOrderService.getRenterOrderByOrderNoAndIsEffective(orderNo);
+        RenterGoodsDetailDTO ownerGoodsDetailDTO = renterGoodsService.getRenterGoodsDetail(renterOrderEntity.getRenterOrderNo(), false);
+        return ownerGoodsDetailDTO.getCarOwnerType();
+    }
+
+
 }
