cc0a070c2bcb4e892a22c34fc2fff7a279d9c8a9
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-04-16 10:56:59 +0800

    租客结算表增加应扣金额

--- order-settle/src/main/java/com/atzuche/order/settle/service/OrderSettleService.java
+++ order-settle/src/main/java/com/atzuche/order/settle/service/OrderSettleService.java
@@ -608,10 +608,10 @@ public class OrderSettleService{
         }
         if(cancelOrderReqDTO.isSettleRenterFlg()){
             OrderStatusEntity orderStatusEntity = orderStatusService.getByOrderNo(orderNo);
-            /*if(orderStatusEntity != null && SettleStatusEnum.SETTLED.getCode().equals(orderStatusEntity.getSettleStatus())){
+            if(orderStatusEntity != null && SettleStatusEnum.SETTLED.getCode().equals(orderStatusEntity.getSettleStatus())){
                 log.info("订单已经结算过orderNo={}",orderNo);
                 return;
-            }*/
+            }
             ownerOrderSettleService.settleOwnerOrderCancel(orderNo,ownerOrderNo);
             renterOrderSettleService.settleRenterOrderCancel(orderNo,renterOrderNo);
         }else{
