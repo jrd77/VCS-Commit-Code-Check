07f81d02c5afa8cf76c2271d6669e6faba0562d3
Andy Liang <andysjtu@163.com>
2020-04-20 14:29:59 +0800

    fix:修改订单钱包的描述错误

--- order-wallet/order-wallet-server/src/main/java/com/atzuche/order/wallet/server/controller/OrderWalletController.java
+++ order-wallet/order-wallet-server/src/main/java/com/atzuche/order/wallet/server/controller/OrderWalletController.java
@@ -1,9 +1,12 @@
 package com.atzuche.order.wallet.server.controller;
 
+import com.atzuche.order.commons.StringUtil;
+import com.atzuche.order.commons.exceptions.InputErrorException;
 import com.atzuche.order.wallet.server.service.WalletService;
 import com.atzuche.order.wallet.api.DeductWalletVO;
 import com.atzuche.order.wallet.api.TotalWalletVO;
 import com.autoyol.commons.web.ResponseData;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,9 +33,13 @@ public class OrderWalletController {
     public ResponseData<DeductWalletVO> deductWallet(@RequestBody DeductWalletVO deductWalletVO){
          Integer reqAmt = deductWalletVO.getAmt();
          if(reqAmt<=0){
-             throw new IllegalArgumentException("amt cannot less zero");
+             throw new InputErrorException("amt 不能小于0");
          }
-         int realAmt = walletService.deductWallet(deductWalletVO.getMemNo(),deductWalletVO.getOrderNo(),deductWalletVO.getAmt());
+         String expDesc= deductWalletVO.getExpDesc();
+         if(StringUtils.isBlank(expDesc)){
+             expDesc="租车消费";
+         }
+         int realAmt = walletService.deductWallet(deductWalletVO.getMemNo(),deductWalletVO.getOrderNo(),deductWalletVO.getAmt(),expDesc);
          deductWalletVO.setAmt(realAmt);
          return ResponseData.success(deductWalletVO);
 
@@ -42,9 +49,13 @@ public class OrderWalletController {
     public ResponseData returnOrChangeWallet(@RequestBody DeductWalletVO deductWalletVO){
         Integer reqAmt = deductWalletVO.getAmt();
         if(reqAmt<=0){
-            throw new IllegalArgumentException("amt cannot less zero");
+            throw new InputErrorException("amt 不能小于0");
         }
-        walletService.returnOrCharge(deductWalletVO.getMemNo(),deductWalletVO.getOrderNo(),deductWalletVO.getAmt());
+        String expDesc= deductWalletVO.getExpDesc();
+        if(StringUtils.isBlank(expDesc)){
+            expDesc="订单返还";
+        }
+        walletService.returnOrCharge(deductWalletVO.getMemNo(),deductWalletVO.getOrderNo(),deductWalletVO.getAmt(),expDesc);
         return ResponseData.success();
 
     }
