31a48f04efdcbd875b17ab4b9cb34b205548acd7
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-04-22 16:33:57 +0800

    车主收益预算和结算合并接口

--- util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/dto/OwnerPreAndSettleIncomRespDTO.java
+++ util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/dto/OwnerPreAndSettleIncomRespDTO.java
@@ -5,8 +5,14 @@ import lombok.Data;
 
 @Data
 public class OwnerPreAndSettleIncomRespDTO {
-    @AutoDocProperty(value = "车主收益")
+    @AutoDocProperty(value = "车主预计收益/结算后并且审核通过收益")
     private Integer ownerIncomAmt;
+
+    //@AutoDocProperty(value = "结算后待审核收益")
+   //private Integer auditingIncomAmt;
+
+    //@AutoDocProperty(value = "车主总收益（当前会员号下的收益）")
+    //private Integer ownerTotalAmt;
 
     @AutoDocProperty(value = "结算状态0：未结算，1:已结算，2：结算失败")
     private Integer settleStatus;
