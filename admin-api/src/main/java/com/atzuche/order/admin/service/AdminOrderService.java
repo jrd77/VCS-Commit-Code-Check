1fde5c5955eb990713f93535b968a690bf0929c6
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-05-06 10:29:26 +0800

    feign优化12

--- admin-api/src/main/java/com/atzuche/order/admin/service/AdminOrderService.java
+++ admin-api/src/main/java/com/atzuche/order/admin/service/AdminOrderService.java
@@ -33,7 +33,7 @@ import com.autoyol.commons.web.ResponseData;
 import com.dianping.cat.Cat;
 import com.dianping.cat.message.Transaction;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
