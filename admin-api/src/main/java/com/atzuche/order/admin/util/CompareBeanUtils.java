601348506f1f0497bec6ee270cd516d07fc8f082
changshu.xie <541399910@qq.com>
2020-05-29 09:54:21 +0800

    提供外部调用接口

--- admin-api/src/main/java/com/atzuche/order/admin/util/CompareBeanUtils.java
+++ admin-api/src/main/java/com/atzuche/order/admin/util/CompareBeanUtils.java
@@ -356,4 +356,23 @@ public class CompareBeanUtils<T> {
         }
     }
 
+    public static boolean compareString(String oldStr,String newStr){
+        if(oldStr == null && newStr == null){
+            return false;
+        }
+        if(oldStr != null && oldStr.equals(newStr)){
+            return false;
+        }
+        if("0".equals(oldStr) && newStr == null){
+            return false;
+        }
+        if("0".equals(newStr) && oldStr == null){
+            return false;
+        }
+        if("0".equals(newStr)  && "0".equals(oldStr)){
+            return false;
+        }
+        return true;
+    }
+
 }
