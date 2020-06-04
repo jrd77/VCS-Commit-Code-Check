ba58f349687ba4ad1021a166f4d9fa05cd4e5004
changshu.xie <541399910@qq.com>
2020-06-04 17:55:53 +0800

    调试

--- admin-api/src/main/java/com/atzuche/order/admin/controller/AdminMemberController.java
+++ admin-api/src/main/java/com/atzuche/order/admin/controller/AdminMemberController.java
@@ -1,7 +1,7 @@
 package com.atzuche.order.admin.controller;
 
-import com.atzuche.order.admin.vo.resp.wallet.WalletBalanceVO;
 import com.atzuche.order.commons.entity.dto.MemberDebtListReqDTO;
+import com.atzuche.order.commons.entity.dto.MemberDebtListResDTO;
 import com.atzuche.order.open.service.FeignCashWithdrawalService;
 import com.autoyol.commons.utils.Page;
 import com.autoyol.commons.web.ResponseData;
@@ -10,7 +10,10 @@ import com.autoyol.doc.annotation.AutoDocVersion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/console")
 @RestController
@@ -23,7 +26,7 @@ public class AdminMemberController {
     private FeignCashWithdrawalService feignCashWithdrawalService;
 
 
-	@AutoDocMethod(description = "会员欠款查看", value = "会员欠款查看", response = WalletBalanceVO.class)
+	@AutoDocMethod(description = "会员欠款查看", value = "会员欠款查看", response = MemberDebtListResDTO.class)
 	@PostMapping("/member/mmeberDebt")
 	public ResponseData walletBalance(@RequestBody MemberDebtListReqDTO req) {
         ResponseData<Page> pageResponseData = feignCashWithdrawalService.queryList(req);
