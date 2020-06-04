95363409c50c94c2d4afc30eb5f612881f9612b3
changshu.xie <541399910@qq.com>
2020-06-04 09:56:45 +0800

    编译不通过

--- util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/orderDetailDto/AccountOwnerIncomeListDTO.java
+++ util-all/order-commons/src/main/java/com/atzuche/order/commons/entity/orderDetailDto/AccountOwnerIncomeListDTO.java
@@ -1,14 +1,27 @@
 package com.atzuche.order.commons.entity.orderDetailDto;
 
 import com.autoyol.doc.annotation.AutoDocProperty;
-import lombok.Data;
 
 import java.io.Serializable;
-@Data
 public class AccountOwnerIncomeListDTO implements Serializable {
-    private static final long serialVersionUID = 1L;
     @AutoDocProperty(value="会员号")
     private String memNo;
     @AutoDocProperty(value="收益总金额")
     private Integer incomeAmt;
+
+    public String getMemNo() {
+        return memNo;
+    }
+
+    public void setMemNo(String memNo) {
+        this.memNo = memNo;
+    }
+
+    public Integer getIncomeAmt() {
+        return incomeAmt;
+    }
+
+    public void setIncomeAmt(Integer incomeAmt) {
+        this.incomeAmt = incomeAmt;
+    }
 }
