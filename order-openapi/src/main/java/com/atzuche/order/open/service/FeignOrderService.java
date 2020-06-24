04112cc150e17ca89606150416d9b31dffc710f6
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-06-24 13:54:55 +0800

    修改地址

--- order-openapi/src/main/java/com/atzuche/order/open/service/FeignOrderService.java
+++ order-openapi/src/main/java/com/atzuche/order/open/service/FeignOrderService.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="order-center-api",url = "http://localhost:1412")
+@FeignClient(name="order-center-api")
 public interface FeignOrderService {
 
     /*
