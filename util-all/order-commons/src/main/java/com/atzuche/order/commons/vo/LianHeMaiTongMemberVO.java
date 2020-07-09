ca305993519c2ac27a6964145b46c9a35750e5f1
bin.zhang <UJMvlFDF8F7C2rI67pxpyYVXwi85tplc>
2020-07-09 16:46:41 +0800

    联合麦通

--- util-all/order-commons/src/main/java/com/atzuche/order/commons/vo/LianHeMaiTongMemberVO.java
+++ util-all/order-commons/src/main/java/com/atzuche/order/commons/vo/LianHeMaiTongMemberVO.java
@@ -15,6 +15,10 @@ public class LianHeMaiTongMemberVO {
     private String memNo;
     @AutoDocProperty("手机号")
     private String phone;
+    @AutoDocProperty("短租订单详情地址")
+    private String ordinaryDetailUrl = "/system/transaction/order/ordinary/detail/orderInfo/{ORDER_NO}";
+    @AutoDocProperty("长租订单详情地址")
+    private String longDetailUrl = "/system/transaction/order/longrent/detail/orderInfo/{ORDER_NO}";
 
     @AutoDocProperty("订单列表信息")
     Page<LianHeMaiTongOrderVO> page;
