42c49cd945c3fa13c7c3fb12097e527f424c71b6
pengcheng.fu <pengcheng.fu@atzuche.com>
2020-07-10 16:38:00 +0800

    提现结果处理

--- order-core-api/src/main/java/com/atzuche/order/coreapi/listener/SecondaryOwnerIncomeToAccountNoticeListener.java
+++ order-core-api/src/main/java/com/atzuche/order/coreapi/listener/SecondaryOwnerIncomeToAccountNoticeListener.java
@@ -1,5 +1,10 @@
 package com.atzuche.order.coreapi.listener;
 
+import com.alibaba.fastjson.JSON;
+import com.atzuche.order.commons.CatConstants;
+import com.atzuche.order.coreapi.entity.mq.OwnerIncomeAmtNoticeMq;
+import com.dianping.cat.Cat;
+import com.dianping.cat.message.Transaction;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -21,5 +26,20 @@ public class SecondaryOwnerIncomeToAccountNoticeListener {
     public void noticeHandle(String message) {
         log.info("Owner income to account notice. context is, message:[{}]", message);
 
+        Transaction t = Cat.newTransaction(CatConstants.RABBIT_MQ_CALL, "上海银行二清资金到账通知处理MQ");
+        try {
+            Cat.logEvent(CatConstants.RABBIT_MQ_METHOD, "SecondaryOwnerIncomeToAccountNoticeListener.noticeHandle");
+            Cat.logEvent(CatConstants.RABBIT_MQ_PARAM, message);
+            OwnerIncomeAmtNoticeMq noticeMq = JSON.parseObject(message, OwnerIncomeAmtNoticeMq.class);
+
+
+            t.setStatus(Transaction.SUCCESS);
+        } catch (Exception e) {
+            t.setStatus(e);
+            log.error("Owner income to account notice. handle err. message:[{}]", message, e);
+            Cat.logError("Owner income to account notice. handle err. message:" + message, e);
+        } finally {
+            t.complete();
+        }
     }
 }
