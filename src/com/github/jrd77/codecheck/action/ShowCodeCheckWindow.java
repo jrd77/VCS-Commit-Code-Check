344c131ce19ff2f12b760834a0bcdeee688584d6
jrd77 <tyxexeUFzjkh>
2021-08-12 10:55:57 +0800

    添加快捷键

--- src/com/github/jrd77/codecheck/action/ShowCodeCheckWindow.java
+++ src/com/github/jrd77/codecheck/action/ShowCodeCheckWindow.java
@@ -2,14 +2,11 @@ package com.github.jrd77.codecheck.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindowManager;
 
 import java.util.Objects;
 
-public class ShowCheckWindow extends AnAction {
+public class ShowCodeCheckWindow extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
@@ -17,6 +14,5 @@ public class ShowCheckWindow extends AnAction {
         ToolWindowManager.getInstance(Objects.requireNonNull(e.getProject())).getToolWindow("预提交代码检查配置").show(()->{
             System.out.println("打开vcsCheck");
         });
-
     }
 }
