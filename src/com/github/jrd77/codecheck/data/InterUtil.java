26675b657ad02b3d4db02e1dd49bdd0f8b08566c
jrd77 <tyxexeUFzjkh>
2021-08-19 13:37:20 +0800

    bug fix:properties getter

--- src/com/github/jrd77/codecheck/data/InterUtil.java
+++ src/com/github/jrd77/codecheck/data/InterUtil.java
@@ -4,6 +4,7 @@ import com.github.jrd77.codecheck.util.Assert;
 import com.github.jrd77.codecheck.util.PropsUtil;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
 /**
@@ -14,17 +15,17 @@ public class InterUtil {
 
     private static Logger logger = Logger.getLogger(InterUtil.class.getName());
 
-    private static final String DEFAULT_LANGUAGE_ZH ="language/zh.properties";
-    private static final String DEFAULT_LANGUAGE_EN ="language/en.properties";
+    private static final String DEFAULT_LANGUAGE_ZH ="language/zh";
+    private static final String DEFAULT_LANGUAGE_EN ="language/en";
     private static final String DEFAULT_LANGUAGE ="zh";
     private static PropsUtil.Props INTERNATIONAL_PROPS =null;
+    private static ResourceBundle bundle=null;
     static {
         Locale aDefault = Locale.getDefault();
-        String pluginPath = getPluginPath();
         if(aDefault.getLanguage().equals(DEFAULT_LANGUAGE)){
-            INTERNATIONAL_PROPS=PropsUtil.getProperties(pluginPath+DEFAULT_LANGUAGE_ZH);
+            bundle = ResourceBundle.getBundle(DEFAULT_LANGUAGE_ZH);
         }else{
-            INTERNATIONAL_PROPS=PropsUtil.getProperties(pluginPath+DEFAULT_LANGUAGE_EN);
+            bundle = ResourceBundle.getBundle(DEFAULT_LANGUAGE_EN);
         }
     }
 
@@ -35,15 +36,7 @@ public class InterUtil {
      */
     public static String getValue(String name){
 
-        Assert.notNull(INTERNATIONAL_PROPS,"can't get language tips file");
-        return INTERNATIONAL_PROPS.getProperty(name);
-    }
-
-
-    private static String getPluginPath(){
-
-        String pluginProject = InterUtil.class.getResource("/").getPath();
-        logger.info(String.format("the plugin path is %s",pluginProject));
-        return pluginProject;
+        Assert.notNull(bundle,"can't get language tips file");
+        return bundle.getString(name);
     }
 }
